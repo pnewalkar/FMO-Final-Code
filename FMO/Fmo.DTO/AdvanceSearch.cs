821b04ba3790759b779a6842bef1fc371ccde3f1
Richa T <richa.a.tiwari@capgemini.com>
2017-03-22 20:09:45 +0530

    Added DTO's RFMO-1216

---
+++ FMO/Fmo.DTO/AdvanceSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fmo.DTO
+{
+    public class AdvanceSearch
+    {
+        public ICollection<DeliveryRoute> DeliveryRoute { get; set; }
+
+        public ICollection<DeliveryPoint> DeliveryPoint { get; set; }
+
+        public ICollection<StreetName> StreetName { get; set; }
+
+        public ICollection<PostCode> PostCode { get; set; }
+
+        public ICollection<PostalAddress> PostalAddress { get; set; }
+
+        public ICollection<NetworkLink> NetworkLink { get; set; }
+
+    }
+}
---
+++ FMO/Fmo.DTO/PostalAddress.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fmo.DTO
+{
+    public class PostalAddress
+    {
+
+        public int Address_Id { get; set; }
+
+        public string PostcodeType { get; set; }
+
+        public string OrganisationName { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string BuildingName { get; set; }
+
+        public short? BuildingNumber { get; set; }
+
+        public string SubBuildingName { get; set; }
+
+        public string Thoroughfare { get; set; }
+
+        public string DependentThoroughfare { get; set; }
+
+        public string DependentLocality { get; set; }
+
+        public string DoubleDependentLocality { get; set; }
+
+        public string PostTown { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string DeliveryPointSuffix { get; set; }
+
+        public string SmallUserOrganisationIndicator { get; set; }
+
+        public int? AddressType_Id { get; set; }
+
+        public int? UDPRN { get; set; }
+
+        public bool? AMUApproved { get; set; }
+
+        public int? AddressStatus_Id { get; set; }
+
+        public string POBoxNumber { get; set; }
+
+    }
+}
