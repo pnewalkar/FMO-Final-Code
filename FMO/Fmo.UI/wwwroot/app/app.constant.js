a9bef96ce22da7f482621623a646aa57e2cfa07e
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-03-22 19:36:39 +0530

    prarameter config appsettings and client side enviroment wise

--- FMO/Fmo.UI/wwwroot/app/app.constant.js
+++ FMO/Fmo.UI/wwwroot/app/app.constant.js
@@ -1,1 +1,15 @@
-﻿
+﻿(function (window) {
+    window.__env = window.__env || {};
+
+    // API url
+    window.__env.apiUrl = 'http://localhost:62032';
+
+    // Base url
+    window.__env.baseUrl = '/';
+
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = false;
+}(this));
+
+MyApp.constant("__env", window.__env);
