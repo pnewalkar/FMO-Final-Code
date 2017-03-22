ff8c71a6068ecd91a9dbdfe42e57fcea433d5e15
cgonsalv <crystal.gonsalves@capgemini.com>
2017-03-22 18:10:42 +0530

    Added unit test projects

---
+++ FMO/Tests/Fmo.BusinessServices.Tests/Services/DeliveryPointBusinessServiceFixture.cs
@@ -0,0 +1,29 @@
+﻿using Fmo.BusinessServices.Interfaces;
+using Fmo.BusinessServices.Services;
+using Fmo.Common.TestSupport;
+using Fmo.DataServices.Repositories.Interface;
+using Moq;
+using NUnit.Framework;
+
+namespace Fmo.BusinessServices.Tests.Services
+{
+    [TestFixture]
+    public class DeliveryPointBusinessServiceFixture : TestFixtureBase
+    {
+        private Mock<ISearchDeliveryPointsRepository> mockDeliveryPointsRepository;
+        private IDeliveryPointBussinessService testCandidate;
+
+        protected override void OnSetup()
+        {
+            mockDeliveryPointsRepository = CreateMock<ISearchDeliveryPointsRepository>();
+            testCandidate = new DeliveryPointBusinessService(mockDeliveryPointsRepository.Object);
+        }
+
+        [Test]
+        public void Test_SearchDeliveryPoints()
+        {
+            testCandidate.SearchDelievryPoints();
+            mockDeliveryPointsRepository.Verify(x => x.SearchDelievryPoints(), Times.Once());
+        }
+    }
+}
