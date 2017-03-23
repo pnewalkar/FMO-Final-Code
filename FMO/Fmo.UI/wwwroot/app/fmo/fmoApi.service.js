61a062a0de6693b34ef518fec393a9f15a26be4b
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-03-23 15:34:27 +0530

    Client side parameter configurations and allowed cors RFMO-321

--- FMO/Fmo.UI/wwwroot/app/fmo/fmoApi.service.js
+++ FMO/Fmo.UI/wwwroot/app/fmo/fmoApi.service.js
@@ -1,34 +1,11 @@
-﻿MyApp.factory('fmoService', ['$http', '__env', function ($http, __env) {
+﻿
+// Just for reference
+angular.module('fmoCommonHome')
+    .factory('fmoService', ['$http', 'GlobalSettings', function ($http, GlobalSettings) {
     var fmoService = {};
-
-    var url = __env;
-    //api.deleteUser = deleteUser;
-
-    
-    //$http({   
-    //    url: 'http://localhost:62032/api/Home/getheroes/',   
-    //    dataType: 'json',   
-    //    method: 'POST',   
-    //    data: { employee: employee },
-    //    headers: {   
-    //        "Content-Type": "application/json"   
-    //    }   
-    //}).success(function (response) {   
-    //    $scope.value = response;   
-    //})   
-    //.error(function (error) {   
-    //    alert(error);   
-    //});   
-
   
     var getUsers = function (objEmployee) {
-
-    
-        //objEmployee = encodeURIComponent(objEmployee);
-        
-        //return $http.post('http://localhost:62032/api/Home/getheroes', JSON.stringify({ name: "jitendra", address: "shirpur", city: "shirpur", mobileno: "9619183061" }), { headers: {"Content-Type":"application/json"}});
-        return $http.post(url.apiUrl + '/api/Home/GetHeroes', objEmployee);
-      
+        return $http.post(GlobalSettings.apiUrl + '/Values', objEmployee);
     };
 
     fmoService.getUsers = getUsers;
