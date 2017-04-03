5e4304c4f650b6962c238813f6113c2ecbacf038
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-04-03 14:58:39 +0530

    Manage Key RFMO-1211

--- FMO/Fmo.UI/wwwroot/app/map-keys/map-keys.controller.js
+++ FMO/Fmo.UI/wwwroot/app/map-keys/map-keys.controller.js
@@ -9,13 +9,6 @@ function MapKeyController($scope, mapStylesFactory, mapService) {
     vm.showKey = showKey;
 
     var activeStyle = mapStylesFactory.getStyle();
-    var spoofStyleObj = function (groupType) {
-        return {
-            "get": function (key) { return this[key]; },
-            "type": "group",
-            "groupType": groupType
-        }
-    }
 
     function showKey(id) {
         if (id == "") {
@@ -39,60 +32,11 @@ function MapKeyController($scope, mapStylesFactory, mapService) {
     }
 
     vm.pointTypes = [
-        //{
-        //    "text": "Selected",
-        //    "id": "",
-        //    "style": mapStylesFactory.getStyle(mapStylesFactory.styleTypes.SELECTEDSTYLE)("deliverypoint")
-        //},
         {
             "text": "Delivery Point",
             "id": "deliverypoint",
             "style": activeStyle("deliverypoint")
         }
-        //,
-        //{
-        //    "text": "Collection Point",
-        //    "id": "collectionpoint",
-        //    "style": activeStyle("collectionpoint")
-        //},
-        //{
-        //    "text": "RMG Point",
-        //    "id": "rmgpoint",
-        //    "style": activeStyle("rmgpoint")
-        //},
-        //{
-        //    "text": "Van/Person",
-        //    "id": "routesim-point",
-        //    "style": activeStyle("routesim-point")
-        //}
-    ];
-
-    vm.areaTypes = [
-        //{
-        //    "text": "Tenement Block",
-        //    "id": "group",
-        //    "style": activeStyle(new spoofStyleObj("Tenement Block"))
-        //},
-        //{
-        //    "text": "Conversion",
-        //    "id": "group",
-        //    "style": activeStyle(new spoofStyleObj("Conversion"))
-        //},
-        //{
-        //    "text": "Complex",
-        //    "id": "group",
-        //    "style": activeStyle(new spoofStyleObj("Complex"))
-        //},
-        //{
-        //    "text": "Skyscraper",
-        //    "id": "group",
-        //    "style": activeStyle(new spoofStyleObj("Skyscraper"))
-        //},
-        //{
-        //    "text": "Service Point",
-        //    "id": "group",
-        //    "style": activeStyle(new spoofStyleObj("Service Point"))
-        //}
     ];
 
     vm.lineTypes = [
@@ -106,11 +50,5 @@ function MapKeyController($scope, mapStylesFactory, mapService) {
             "id": "route",
             "style": activeStyle("route")
         }
-       // ,
-       //{
-       //    "text": "Path Link",
-       //    "id": "pathlink",
-       //    "style": activeStyle("pathlink")
-       //}
     ];
 };
