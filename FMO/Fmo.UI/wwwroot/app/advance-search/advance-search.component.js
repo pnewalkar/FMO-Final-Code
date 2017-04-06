2049f4be79a7fcbc10953b0ed4f18201aa06c765
rictiwar <rictiwar@DIN19001008>
2017-04-06 16:21:45 +0530

    Committed advance search changes

---
+++ FMO/Fmo.UI/wwwroot/app/advance-search/advance-search.component.js
@@ -0,0 +1,6 @@
+﻿angular.module('advanceSearch')
+  .component('advanceSearch', {
+      restrict: 'E',
+      scope: {},
+      templateUrl: './advance-search/advance-search.template.html'
+  });
---
+++ FMO/Fmo.UI/wwwroot/app/advance-search/advance-search.factory.js
@@ -0,0 +1,16 @@
+﻿
+angular.module('advanceSearch')
+    .factory('advanceSearchService',
+    ['$http', 'GlobalSettings', function ($http, GlobalSettings) {
+        var advanceSearchApiService = {};
+        debugger;
+        //layersApiService.fetchDeliveryPoints = function () {
+        //    return $http.get(GlobalSettings.apiUrl + '/deliveryPoints/fetchDeliveryPoint');
+        //};
+
+        //layersApiService.fetchAccessLinks = function () {
+        //    return $http.get(GlobalSettings.apiUrl + '/accessLinks/fetchAccessLink');
+        //};
+        return advanceSearchApiService;
+
+    }]);
