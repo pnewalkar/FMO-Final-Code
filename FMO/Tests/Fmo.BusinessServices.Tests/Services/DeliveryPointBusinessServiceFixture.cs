3da1e761736c3fc2b99b0db3fdbe9ba906c50572
rictiwar <rictiwar@DIN19001008>
2017-04-07 12:30:08 +0530

    Commit advance search UI changes

--- FMO/Tests/Fmo.BusinessServices.Tests/Services/DeliveryPointBusinessServiceFixture.cs
+++ FMO/Tests/Fmo.BusinessServices.Tests/Services/DeliveryPointBusinessServiceFixture.cs
@@ -2,8 +2,10 @@
 using Fmo.BusinessServices.Services;
 using Fmo.Common.TestSupport;
 using Fmo.DataServices.Repositories.Interfaces;
+using Fmo.DTO;
 using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Fmo.BusinessServices.Tests.Services
 {
@@ -15,8 +17,14 @@ namespace Fmo.BusinessServices.Tests.Services
 
         protected override void OnSetup()
         {
+
+            List<DeliveryPointDTO> lstDeliveryPointDTO = new List<DeliveryPointDTO>();
+           
             mockDeliveryPointsRepository = CreateMock<IDeliveryPointsRepository>();
             testCandidate = new DeliveryPointBusinessService(mockDeliveryPointsRepository.Object);
+            mockDeliveryPointsRepository.Setup(n=>n.GetDeliveryPoints1(It.IsAny<string>())).Returns
         }
+
+
     }
 }
