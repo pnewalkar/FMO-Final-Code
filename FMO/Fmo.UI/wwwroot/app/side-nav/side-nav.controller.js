c8906e6c15ed66ef62ef8347539216ed2ba566a2
CORP\jimali <jitendra.mali@capgemini.com>
2017-04-10 17:18:44 +0530

    call routesimulation state in side nav controller

--- FMO/Fmo.UI/wwwroot/app/side-nav/side-nav.controller.js
+++ FMO/Fmo.UI/wwwroot/app/side-nav/side-nav.controller.js
@@ -1,33 +1,28 @@
 angular
     .module('sideNav')
-    .controller("sideNavController", ['$stateParams', '$state','$scope', 'routeLogService', '$mdSidenav', '$mdDialog', sideNavController])
+    .controller("sideNavController", ['$stateParams', '$state', '$scope', 'routeLogService', '$mdSidenav', '$mdDialog', sideNavController])
 
 function sideNavController($stateParams, $state, $scope, routeLogService, $mdSidenav, $mdDialog) {
-        vm = this;
-        vm.routeLog =routeLog;
-        vm.openModalPopup=openModalPopup;
-        
-        vm.routeSimulation = routeSimulation;
+    vm = this;
+    vm.routeLog = routeLog;
+    vm.openModalPopup = openModalPopup;
 
-
-
-
-        function routeSimulation(selectedDeliveryUnit) {
-            debugger;
-            $state.go("routeSimulation", { selectedUnit: selectedDeliveryUnit });
-        }
-        $scope.toggleSideNav = function() {
-        $mdSidenav('left').toggle();
-                }
-        
-    function routeLog(){
-        var setting =routeLogService.routeLog(); 
-        vm.openModalPopup(setting);
-         ///vm.openModalPopup("Test");
+    vm.routeSimulation = routeSimulation;
+    function routeSimulation(selectedDeliveryUnit) {
+        debugger;
+        $state.go("routeSimulation", { selectedUnit: selectedDeliveryUnit });
     }
-        
-        function openModalPopup(modalSetting) {
+    $scope.toggleSideNav = function () {
+        $mdSidenav('left').toggle();
+    }
+
+    function routeLog() {
+        var setting = routeLogService.routeLog();
+        vm.openModalPopup(setting);
+    }
+
+    function openModalPopup(modalSetting) {
         var popupSetting = modalSetting;
-      $mdDialog.show(popupSetting)
+        $mdDialog.show(popupSetting)
     };
 };
