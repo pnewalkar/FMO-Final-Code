0ea02a502160fe1035daa594ad78e50edbbd345b
Veerendra Poojary <veerendra.poojary@capgemini.com>
2017-04-11 21:12:25 +0530

    Rename FMO.Batch.FileLoader to Fmo.Batch.FileLoader.

--- FMO/FMO.Batch.FileLoader/Properties/AssemblyInfo.cs
+++ FMO/FMO.Batch.FileLoader/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FMO.Batch.FileLoader")]
+[assembly: AssemblyTitle("Fmo.Batch.FileLoader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FMO.Batch.FileLoader")]
+[assembly: AssemblyProduct("Fmo.Batch.FileLoader")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
