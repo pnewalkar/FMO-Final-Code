141d01fe8269745292c1b5e4bac3b9aae523083a
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-04-14 19:02:08 +0530

    Basic Search RFMO 1213

--- FMO/Fmo.UI/wwwroot/app/search/search.controller.js
+++ FMO/Fmo.UI/wwwroot/app/search/search.controller.js
@@ -3,16 +3,16 @@ angular.module('search')
    .controller('SearchController', SearchController);
 
 function SearchController(searchApiService, $scope, $state, mapFactory, mapStylesFactory) {
-    var self = this;
+    var vm = this;
 
-    self.resultSet = resultSet;
-    self.presEnter = presEnter;
-    self.OnChangeItem = OnChangeItem;
+    vm.resultSet = resultSet;
+    vm.onEnterKeypress = onEnterKeypress;
+    vm.OnChangeItem = OnChangeItem;
 
     function querySearch(query) {
         searchApiService.basicSearch(query).then(function (response) {
-            self.resultscount = response.data.searchCounts;
-            self.results = response.data.searchResultItems
+            vm.resultscount = response.data.searchCounts;
+            vm.results = response.data.searchResultItems;
         });
     }
 
@@ -21,18 +21,18 @@ function SearchController(searchApiService, $scope, $state, mapFactory, mapStyle
             querySearch(query);
         }
         else {
-            self.results = {};
+            vm.results = {};
         }
     }
 
-    function presEnter(searchText) {
+    function onEnterKeypress(searchText) {
         if (searchText.length > 3) {
-            if (self.results.length === 1) {
-                OnChangeItem(self.results);
+            if (vm.results.length === 1) {
+                OnChangeItem(vm.results);
             }
         }
         else {
-            self.results = [{ displayText: "At least three characters must be input for a Search", type: "Warning" }];
+            vm.results = [{ displayText: "At least three characters must be input for a Search", type: "Warning" }];
         }
     }
 
