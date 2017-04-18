1225a1ae59963f4d164ec5fb016e31db2ae7559b
cgonsalv <crystal.gonsalves@capgemini.com>
2017-04-18 21:14:59 +0530

    Role base Authorization RFMO-312

---
+++ FMO/Fmo.UI/wwwroot/app/manage-access/manage-access.controller.js
@@ -0,0 +1,38 @@
+﻿'use strict';
+
+angular
+    .module('manageAccess')
+    .controller('manageAccessController', manageAccessController);
+
+manageAccessController.$inject = ['$stateParams', 'manageAccessService', '$location'];
+
+function manageAccessController($stateParams, manageAccessService, $location) {
+    /* jshint validthis:true */
+    var vm = this;
+    var username = $location;
+    activate();
+
+    function activate() {
+        debugger;
+        var userName = getParameterValues('username');
+        var unitGuid = getParameterValues('unitguid');
+
+        if (userName) {
+            vm.userdata = "username=" + userName + "&unitguid=" + unitGuid;
+            manageAccessService.getToken(vm.userdata).then(function (response) {
+                var accessData = response.data;
+                sessionStorage.setItem("roleAccessData", accessData)
+            });
+        }
+    }
+
+    function getParameterValues(param) {
+        var url = window.location.search.slice(window.location.search.indexOf('?') + 1).split('&');
+        for (var i = 0; i < url.length; i++) {
+            var urlparam = url[i].split('=');
+            if (urlparam[0] == param) {
+                return urlparam[1];
+            }
+        }
+    }
+}
