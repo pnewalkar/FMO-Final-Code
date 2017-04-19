aa9e30a807e8d7e06761025853b58521cbd4d8d3
CORP\jimali <jitendra.mali@capgemini.com>
2017-04-19 11:51:35 +0530

    manageaccessbusinessService

--- FMO/Fmo.UI/wwwroot/app/manage-access/manage-access.controller.js
+++ FMO/Fmo.UI/wwwroot/app/manage-access/manage-access.controller.js
@@ -4,35 +4,15 @@ angular
     .module('manageAccess')
     .controller('manageAccessController', manageAccessController);
 
-manageAccessController.$inject = ['$stateParams', 'manageAccessService', '$location'];
+manageAccessController.$inject = ['$stateParams', 'manageAccessBusinessService'];
 
-function manageAccessController($stateParams, manageAccessService, $location) {
+function manageAccessController($stateParams,manageAccessBusinessService) {
     /* jshint validthis:true */
     var vm = this;
-    var username = $location;
+    vm.activate = activate;
     activate();
-
-    function activate() {
-        debugger;
-        var userName = getParameterValues('username');
-        var unitGuid = getParameterValues('unitguid');
-
-        if (userName) {
-            vm.userdata = "username=" + userName + "&unitguid=" + unitGuid;
-            manageAccessService.getToken(vm.userdata).then(function (response) {
-                var accessData = response.data;
-                sessionStorage.setItem("roleAccessData", accessData)
-            });
-        }
-    }
-
-    function getParameterValues(param) {
-        var url = window.location.search.slice(window.location.search.indexOf('?') + 1).split('&');
-        for (var i = 0; i < url.length; i++) {
-            var urlparam = url[i].split('=');
-            if (urlparam[0] == param) {
-                return urlparam[1];
-            }
-        }
+    function activate()
+    {
+        manageAccessBusinessService.activate(null);
     }
 }
