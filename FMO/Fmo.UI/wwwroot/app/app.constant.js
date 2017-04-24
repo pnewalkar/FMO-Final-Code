abe4cc79fb10a0ddc0c886c93091ec3bd4bae162
cgonsalv <crystal.gonsalves@capgemini.com>
2017-04-24 13:47:26 +0530

    RFMO-310 Implemented code review comments

--- FMO/Fmo.UI/wwwroot/app/app.constant.js
+++ FMO/Fmo.UI/wwwroot/app/app.constant.js
@@ -16,6 +16,10 @@ else {
 }
 
 GlobalSettings.vectorMapUrl = "http://api.tiles.mapbox.com/v4/digitalglobe.nal0g75k/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNpcGg5dHkzYTAxM290bG1kemJraHU5bmoifQ.CHhq1DFgZPSQQC-DYWpzaQ";
+GlobalSettings.unitBoundaryLayerName = "Unit Boundary";
+GlobalSettings.deliveryPointLayerName = "Delivery Points";
+GlobalSettings.accessLinkLayerName = "Access Links";
+GlobalSettings.roadLinkLayerName = "Roads";
 
 angular.module('FMOApp')
 .constant("GlobalSettings", GlobalSettings);
