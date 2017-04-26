99ded1d67bf7320b0d50ea24e55663809fc580e7
Veerendra Poojary <veerendra.poojary@capgemini.com>
2017-04-26 15:17:35 +0530

    NYB issue fix RFMO-268.

--- FMO/Fmo.NYBLoader/NYBLoader.cs
+++ FMO/Fmo.NYBLoader/NYBLoader.cs
@@ -65,7 +65,7 @@ namespace Fmo.NYBLoader
 
                         //Remove Channel Island and Isle of Man Addresses are ones where the Postcode starts with one of: GY, JE or IM and Invalid records
 
-                        lstAddressDetails = lstAddressDetails.SkipWhile(n => (n.Postcode.StartsWith(PostCodePrefix.GY.ToString()) || n.Postcode.StartsWith(PostCodePrefix.JE.ToString()) || n.Postcode.StartsWith(PostCodePrefix.IM.ToString()))).ToList();
+                        lstAddressDetails = lstAddressDetails.Where(n => !n.Postcode.StartsWith(PostCodePrefix.GY.ToString(),StringComparison.OrdinalIgnoreCase) && !n.Postcode.StartsWith(PostCodePrefix.JE.ToString(), StringComparison.OrdinalIgnoreCase) && !n.Postcode.StartsWith(PostCodePrefix.IM.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
                     }
                 }
             }
