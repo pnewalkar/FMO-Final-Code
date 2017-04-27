8bea6435b97e6cb5a659e4d932428746738ac7d9
CORP\jimali <jitendra.mali@capgemini.com>
2017-04-27 19:40:34 +0530

    Route Simulation & Switch Unit  Related Bug

---
+++ FMO/Fmo.BusinessServices/Services/ReferenceDataBusinessService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fmo.BusinessServices.Interfaces;
+using Fmo.DataServices.Repositories.Interfaces;
+using Fmo.DTO;
+
+namespace Fmo.BusinessServices.Services
+{
+    public class ReferenceDataBusinessService : IReferenceDataBusinessService
+    {
+        private IReferenceDataCategoryRepository referenceDataCategoryRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReferenceDataBusinessService"/> class and other classes.
+        /// </summary>
+        /// <param name="referenceDataCategoryRepository">IReferenceDataCategoryRepository reference</param>   
+        public ReferenceDataBusinessService(IReferenceDataCategoryRepository referenceDataCategoryRepository)
+        {
+            this.referenceDataCategoryRepository = referenceDataCategoryRepository;
+        }
+
+        public List<ReferenceDataDTO> FetchRouteLogSelectionType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ReferenceDataDTO> FetchRouteLogStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
