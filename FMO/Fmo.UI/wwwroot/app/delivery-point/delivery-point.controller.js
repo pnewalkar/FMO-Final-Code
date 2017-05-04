d3dd5749ba0f710d758d31e43612c14d7aac0743
rictiwar <rictiwar@DIN19001008>
2017-05-04 14:04:53 +0530

    RFMO-161 changes of UI

--- FMO/Fmo.UI/wwwroot/app/delivery-point/delivery-point.controller.js
+++ FMO/Fmo.UI/wwwroot/app/delivery-point/delivery-point.controller.js
@@ -1,23 +1,55 @@
 angular
     .module('deliveryPoint')
-    .controller("DeliveryPointController", ['$scope','$mdDialog','deliveryPointService', DeliveryPointController])
-function DeliveryPointController($scope,$mdDialog,deliveryPointService) {
+    .controller("DeliveryPointController", ['$scope', '$mdDialog', 'deliveryPointService', 'deliveryPointApiService', DeliveryPointController])
+function DeliveryPointController($scope, $mdDialog, deliveryPointService, deliveryPointApiService) {
     var vm = this;
-    
+    vm.resultSet = resultSet;
+    vm.querySearch = querySearch;
     vm.deliveryPoint = deliveryPoint;
     vm.openModalPopup = openModalPopup;
-    vm.closeWindow = closeWindow;    
-    
+    vm.closeWindow = closeWindow;
+    vm.OnChangeItem = OnChangeItem;
+
+    querySearch
+    function querySearch(query) {
+        debugger;
+        deliveryPointApiService.GetDeliveryPointsResultSet(query).then(function (response) {
+                        debugger;
+                        vm.results = response.data;
+
+
+
+                    });
+    }
+
     function deliveryPoint() {
         var deliveryPointTemplate = deliveryPointService.deliveryPoint();
         vm.openModalPopup(deliveryPointTemplate);
     }
-    
+
+    function resultSet(query) {
+        if (query.length >= 3) {
+            querySearch(query);
+        }
+        else {
+            vm.results = {};
+            vm.resultscount = { 0: { count: 0 } };
+        }
+    }
+
+
+
+    function OnChangeItem(selectedItem) {
+
+        vm.searchText = selectedItem;
+        vm.results = {};
+    }
+
     function openModalPopup(modalSetting) {
         var popupSetting = modalSetting;
         $mdDialog.show(popupSetting)
     }
-    
+
     function closeWindow() {
         $mdDialog.hide(vm.close);
     }
