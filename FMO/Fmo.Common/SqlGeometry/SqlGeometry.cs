34d18b41b57e9ca300761f323b363d1d10779445
Rahul.Upadhyay <rahul.upadhyay@capgemini.com>
2017-05-11 12:32:03 +0530

    RFMO-202 : Automatic access link creation

---
+++ FMO/Fmo.Common/SqlGeometry/SqlGeometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Types;
+
+namespace Fmo.Common.SqlGeometryExtension
+{
+    public static class SqlGeometryExtension
+    {
+        public static DbGeometry ToDbGeometry(this SqlGeometry sqlGeometry)
+        {
+            return DbGeometry.FromBinary(sqlGeometry.STAsBinary().Buffer);
+        }
+
+        public static SqlGeometry ToSqlGeometry(this DbGeometry dbGeometry)
+        {
+            return SqlGeometry.STGeomFromWKB(new SqlBytes(dbGeometry.AsBinary()), dbGeometry.CoordinateSystemId);
+        }
+    }
+}
