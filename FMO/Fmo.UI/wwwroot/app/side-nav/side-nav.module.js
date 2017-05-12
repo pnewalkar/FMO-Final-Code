12615fc086a2f3aa504e79dcbbc82a06f09c9282
rictiwar <rictiwar@DIN19001008>
2017-05-12 16:57:05 +0530

    RFMO-211 UI changes commited

--- FMO/Fmo.UI/wwwroot/app/side-nav/side-nav.module.js
+++ FMO/Fmo.UI/wwwroot/app/side-nav/side-nav.module.js
@@ -1,4 +1,5 @@
 angular.module('sideNav',['simulation',
                           'routeLog',
-                          'deliveryPoint'
+                          'deliveryPoint',
+                          'accessLink'
                          ]);
