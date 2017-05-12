e147bf281f9c6b0ecd33102bd85a287865e3b765
rictiwar <rictiwar@DIN19001008>
2017-05-12 17:08:07 +0530

    UI Files added

---
+++ FMO/Fmo.UI/wwwroot/app/access-link/access-link.component.js
@@ -0,0 +1,8 @@
+﻿angular.module('accessLink')
+  .component('accessLink', {
+      restrict: 'E',
+      scope: {},
+      templateUrl: './access-link/acccess-link.template.html',
+      controller: 'AccessLinkController as vm'
+  });
+
