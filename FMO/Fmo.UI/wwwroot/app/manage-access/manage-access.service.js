343a4b502f1d2d3e799dfdc033db628e01ae9be9
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-05-17 14:18:06 +0530

    Rename files and folder Web-UI Code Refactor

--- FMO/Fmo.UI/wwwroot/app/manage-access/manage-access.service.js
+++ FMO/Fmo.UI/wwwroot/app/manage-access/manage-access.service.js
@@ -1,30 +1,68 @@
-﻿'use strict';
+﻿
+angular.module('manageAccess')
+    .factory('manageAccessBusinessService', manageAccessBusinessService);
+manageAccessBusinessService.$inject = ['$stateParams', '$state', 'manageAccessService', '$location', 'GlobalSettings', '$window'];
 
-angular
-    .module('manageAccess')
-    .factory('manageAccessService', manageAccessService);
+function manageAccessBusinessService($stateParams, $state, manageAccessService, $location, GlobalSettings,$window) {
+    var vm = this;
 
-manageAccessService.$inject = ['$http', '$q', 'GlobalSettings'];
-
-function manageAccessService($http, $q, GlobalSettings) {
-    var service = {
-        getToken: getToken
+    return {
+        activate: activate,
+        getParameterValues: getParameterValues
     };
 
-    return service;
+    function activate(unitGuid) {
+        var aValue = sessionStorage.getItem('authorizationData');
+        if (unitGuid) {
 
-    function getToken(userdata) {
-        var deferred = $q.defer();
+            var jobject = angular.fromJson(aValue)
+            vm.userdata = "username=" + jobject.userName + "&unitguid=" + unitGuid;
+        }
+        else {
+            var userName = getParameterValues('username');
+            vm.userdata = "username=" + userName + "&unitguid=" + unitGuid;
+            if (angular.isUndefined(userName)) {
+                if (aValue) {
+                    return;
+                }
+                else {
+                    alert("User Name not provided.")
+                    return;
+                }
+            }
+            else {
+                if (aValue) {
+                    var jobject = angular.fromJson(aValue)
+                    if (jobject.userName !== userName) {
+                        sessionStorage.clear();
+                    }
+                }
+            }
+        }
 
-        $http.post(GlobalSettings.apiUrl + '/token', userdata, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
-            deferred.resolve(response);
+        manageAccessService.getToken(vm.userdata).then(function (response) {
+            var accessData = response.data;
+            if (response.access_token) {
+                sessionStorage.clear();
+                sessionStorage.setItem("authorizationData", angular.toJson({ token: response.access_token, userName: response.username[0], unitGuid: unitGuid }));
+                sessionStorage.setItem("roleAccessData", angular.toJson((response.roleActions)));
+                if (unitGuid) {
+                    $window.location.href = GlobalSettings.indexUrl;
+                } else if (response.access_token || angular.isDefined(response.access_token)) {
+                    $window.location.href = GlobalSettings.indexUrl;
+                }
+            }
 
-        }).error(function (err, status) {
-            alert(err);
-            deferred.reject(err);
         });
+    }
 
-        return deferred.promise;
-
+    function getParameterValues(param) {
+        var url = $window.location.search.slice($window.location.search.indexOf('?') + 1).split('&');
+        for (var i = 0; i < url.length; i++) {
+            var urlparam = url[i].split('=');
+            if (urlparam[0] == param) {
+                return urlparam[1];
+            }
+        }
     }
 }
