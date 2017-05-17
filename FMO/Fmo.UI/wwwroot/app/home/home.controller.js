04738d3ffec9dbfe052253c77a84d0bf2e6cd985
supriya bhosale <supriya-suresh.bhosale@capgemini.com>
2017-05-17 21:16:54 +0530

    Home controller code refactoring

--- FMO/Fmo.UI/wwwroot/app/home/home.controller.js
+++ FMO/Fmo.UI/wwwroot/app/home/home.controller.js
@@ -1,9 +1,13 @@
-angular.module('home')
-    .controller('HomeController', ['$scope', '$mdSidenav', HomeController]);
-function HomeController($scope, $mdSidenav) {
+angular
+    .module('home')
+    .controller('HomeController', [
+        '$mdSidenav',
+        HomeController]);
+
+function HomeController(
+    $mdSidenav) {
+
     var vm = this;
-    vm.deliveryUnit = vm.selectedDeliveryUnit;
-    var title = vm.contextTitle;
     vm.toggleSideNav = toggleSideNav;
 
     function toggleSideNav() {
