c58ba90a8fc45f1a6b2e1e076d3b782873348aff
CORP\jimali <jitendra.mali@capgemini.com>
2017-05-18 12:47:40 +0530

    Unit Selector- Defered controller logic move to business Service.

--- FMO/Fmo.UI/wwwroot/app/unit-selector/unit-selector.controller.js
+++ FMO/Fmo.UI/wwwroot/app/unit-selector/unit-selector.controller.js
@@ -1,6 +1,6 @@
 angular.module('unitSelector')
-.controller('UnitSelectorController', ['$scope', '$stateParams', '$state', '$filter', 'unitSelectorAPIService', 'mapFactory', 'manageAccessBusinessService', UnitSelectorController]);
-function UnitSelectorController($scope, $stateParams, $state, $filter, unitSelectorAPIService, mapFactory, manageAccessBusinessService) {
+.controller('UnitSelectorController', ['$filter', 'unitSelectorService', 'unitSelectorAPIService', 'mapFactory', 'manageAccessBusinessService', UnitSelectorController]);
+function UnitSelectorController($filter, unitSelectorService, unitSelectorAPIService, mapFactory, manageAccessBusinessService) {
     var vm = this;
     vm.BindData = BindData;
     vm.DeliveryUnit = DeliveryUnit;
@@ -11,45 +11,20 @@ function UnitSelectorController($scope, $stateParams, $state, $filter, unitSelec
 
     function DeliveryUnit() {
         vm.selectedDeliveryUnit = vm.selectedUser;
-        manageAccessBusinessService.activate(vm.selectedDeliveryUnit.id);
-        updateMapAfterUnitChange(vm.selectedDeliveryUnit);
-
+        unitSelectorService.DeliveryUnit(vm.selectedDeliveryUnit);
     }
 
     function BindData() {
-        if (vm.deliveryRouteUnit.length === 0) {
-            var authData = sessionStorage.getItem('authorizationData');
-            authData = angular.fromJson(authData);
-            if (authData.unitGuid) {
-                unitSelectorAPIService.getDeliveryUnit().then(function (response) {
-                    if (response) {
-                        vm.deliveryRouteUnit = response;
-                        var newTemp = $filter("filter")(vm.deliveryRouteUnit, { id: authData.unitGuid });
-                        vm.selectedUser = newTemp[0];
-                        vm.selectedDeliveryUnit = vm.selectedUser;
-                        updateMapAfterUnitChange(vm.selectedDeliveryUnit);
-                    }
-                });
+        debugger;
 
-            } else {
-                unitSelectorAPIService.getDeliveryUnit().then(function (response) {
-                    if (response) {
-                        if (response.length === 1)
-                        {
-                            vm.isDeliveryUnitDisabled = true;
-                        }
-                        vm.deliveryRouteUnit = response;
-                        vm.selectedUser = vm.deliveryRouteUnit[0];
-                        vm.selectedDeliveryUnit = vm.selectedUser;
-                        updateMapAfterUnitChange(vm.selectedDeliveryUnit);
-                    }
-                });
-            }
-        }
-    }
+        var response = unitSelectorService.BindData(vm.deliveryRouteUnit).then(function (response) {
+            debugger;
+            vm.deliveryRouteUnit = response;
+            vm.selectedUser = vm.deliveryRouteUnit[0];
+            vm.selectedDeliveryUnit = vm.selectedUser;
+           // vm.isDeliveryUnitDisabled = response.isDeliveryUnitDisabled;
+        });
 
-    function updateMapAfterUnitChange(selectedUnit) {
-        mapFactory.setUnitBoundaries(selectedUnit.boundingBox, selectedUnit.boundingBoxCenter, selectedUnit.unitBoundaryGeoJSONData);
     }
 }
 
