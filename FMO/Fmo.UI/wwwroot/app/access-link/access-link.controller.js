00c3c60f6cff0b911fc8cca39197eae778bf55cb
Rahul.Upadhyay <rahul.upadhyay@capgemini.com>
2017-05-18 12:59:44 +0530

    RFMO-212 : UI changes

--- FMO/Fmo.UI/wwwroot/app/access-link/access-link.controller.js
+++ FMO/Fmo.UI/wwwroot/app/access-link/access-link.controller.js
@@ -1,13 +1,25 @@
 ﻿angular
     .module('accessLink')
     .controller("AccessLinkController",
-    [       
+    [
+        'accessLinkApiService',
         '$scope',
         '$mdDialog'
         , AccessLinkController])
-function AccessLinkController(  
+function AccessLinkController(
+    accessLinkApiService,
     $scope,
     $mdDialog 
 ) {
-  
+    vm.createAccessLink = createAccessLink;
+
+    function createAccessLink() {
+        debugger;
+        var accessLink = null;
+
+        accessLinkApiService.createAccessLink(accessLink).then(function (response) {
+            debugger;
+            alert.message(response);            
+        });
+    }
 };
--- FMO/Fmo.UI/wwwroot/app/access-link/access-link.factory.js
+++ FMO/Fmo.UI/wwwroot/app/access-link/access-link.factory.js
@@ -4,4 +4,17 @@ deliveryPointApiService.$inject = ['$http', 'GlobalSettings', '$q'];
 function accessLinkApiService($http, GlobalSettings, $q) {
     var accessLinkApiService = {};
 
-    }
+    accessLinkApiService.CreateAccessLink = function (accessLinkDTO) {
+        var deferred = $q.defer();
+
+        $http.post(GlobalSettings.apiUrl + '/accessLink/CreateManualAccessLink/', accessLinkDTO).success(function (response) {
+            deferred.resolve(response);
+
+        }).error(function (err, status) {
+
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    };
+}
