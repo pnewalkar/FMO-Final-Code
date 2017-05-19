6fb246fb44ebbd7521f492881031b5a26b05d103
skashalk <swapna.kashalkar@capgemini.com>
2017-05-19 17:44:28 +0530

    RFMO-1896 map-style factory refactoring

--- FMO/Fmo.UI/wwwroot/app/map-view/map-styles.factory.js
+++ FMO/Fmo.UI/wwwroot/app/map-view/map-styles.factory.js
@@ -7,7 +7,6 @@ function MapStylesFactory() {
     var ACTIVESTYLE = 0;
     var INACTIVESTYLE = 1;
     var SELECTEDSTYLE = 2;
-
     var blockStyles = {};
     var currentColor = 0;
 
@@ -18,6 +17,7 @@ function MapStylesFactory() {
     var fill = new ol.style.Fill({
         color: 'rgba(255,255,255,0.4)'
     });
+
     var stroke = new ol.style.Stroke({
         color: '#3399CC',
         width: 1.25
@@ -65,15 +65,6 @@ function MapStylesFactory() {
         })
     });
 
-    var measureStyle = new ol.style.Style({
-        fill: whiteFill,
-        stroke: new ol.style.Stroke({
-            color: 'rgba(0, 0, 0, 0.5)',
-            lineDash: [10, 10],
-            width: 2
-        })
-    });
-
     var deliveryPointStyle = new ol.style.Style({
         text: new ol.style.Text({
             text: '\uf041',
@@ -89,80 +80,6 @@ function MapStylesFactory() {
         })
     });
 
-    var collectionPointStyle = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf041',
-            font: 'normal 18px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: '#0892cb',
-            }),
-            stroke: new ol.style.Stroke({
-                color: '#000',
-                width: 2
-            })
-        })
-    });
-
-    var rmgPointStyle = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf041',
-            font: 'normal 18px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: '#cb912b',
-            }),
-            stroke: new ol.style.Stroke({
-                color: '#000',
-                width: 2
-            })
-        })
-    });
-
-    var routeSimPointStyle = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf1b9',
-            font: 'normal 35px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: 'rgb(218,32,42)', // RMG Red
-            }),
-            stroke: new ol.style.Stroke({
-                color: 'black',
-                width: 2
-            })
-        })
-    });
-
-    var routeSimPersonStyle = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf007',
-            font: 'normal 35px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: 'rgb(218,32,42)', // RMG Red
-            }),
-            stroke: new ol.style.Stroke({
-                color: 'black',
-                width: 2
-            })
-        })
-    });
-
-    var inactiveMeasureStyle = new ol.style.Style({
-        fill: whiteFill,
-        image: new ol.style.Circle({
-            fill: fill,
-            stroke: inactivestroke,
-            radius: 5
-        }),
-        stroke: new ol.style.Stroke({
-            color: 'rgba(0, 0, 0, 0.5)',
-            lineDash: [10, 10],
-            width: 2
-        })
-    });
-
     var inactivePointStyle = new ol.style.Style({
         text: new ol.style.Text({
             text: '\uf041',
@@ -173,36 +90,6 @@ function MapStylesFactory() {
             }),
             stroke: new ol.style.Stroke({
                 color: '#c4c4c4',
-                width: 2
-            })
-        })
-    });
-
-    var inactiveRoutesimPoint = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf1b9',
-            font: 'normal 35px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: 'rgb(218,32,42)', // RMG Red
-            }),
-            stroke: new ol.style.Stroke({
-                color: 'black',
-                width: 2
-            })
-        })
-    });
-
-    var inactiveRoutesimPerson = new ol.style.Style({
-        text: new ol.style.Text({
-            text: '\uf007',
-            font: 'normal 35px FontAwesome',
-            textBaseline: 'Bottom',
-            fill: new ol.style.Fill({
-                color: 'rgb(218,32,42)', // RMG Red
-            }),
-            stroke: new ol.style.Stroke({
-                color: 'black',
                 width: 2
             })
         })
@@ -230,7 +117,6 @@ function MapStylesFactory() {
             width: 2
         })
     });
-
 
     var inactiveDefaultStyle = new ol.style.Style({
         image: new ol.style.Circle({
@@ -284,14 +170,6 @@ function MapStylesFactory() {
         })
     });
 
-    var servicePointStyle = new ol.style.Style({
-        fill: whiteFill,
-        stroke: new ol.style.Stroke({
-            color: 'rgba(245, 25, 223, 1)',
-            width: 2
-        })
-    });
-
     var selectedConversionStyle = new ol.style.Style({
         fill: whiteFill,
         stroke: new ol.style.Stroke({
@@ -324,14 +202,6 @@ function MapStylesFactory() {
         })
     });
 
-    var selectedServicePointStyle = new ol.style.Style({
-        fill: whiteFill,
-        stroke: new ol.style.Stroke({
-            color: 'rgba(245, 25, 223, 1)',
-            width: 4
-        })
-    });
-
     return {
         styleTypes: { ACTIVESTYLE: ACTIVESTYLE, INACTIVESTYLE: INACTIVESTYLE, SELECTEDSTYLE: SELECTEDSTYLE },
         getStyle: getStyle
@@ -350,60 +220,6 @@ function MapStylesFactory() {
         }
     }
 
-    function getRouteMaintenanceStyle(feature) {
-        var colours = ["green", "maroon", "blue", "black", "orange", "red", "purple", "deeppink", "darkslategray"];
-        var colour = colours[feature.getId() % colours.length];
-
-        var blockStyleOl = new ol.style.Style({
-            stroke: new ol.style.Stroke({
-                color: colour,
-                width: 3
-            })
-        });
-
-        var blockId = angular.isDefined(feature.get) ? feature.getId() : feature;
-        if (blockStyles[blockId]) {
-            blockStyleOl = blockStyles[blockId];
-        } else {
-            blockStyles[blockId] = blockStyleOl;
-        }
-        return blockStyleOl;
-    }
-
-    function blockStyle(feature) {
-        var colours = ["green", "purple", "blue", "brown", "orange", "red"];
-        var colour = colours[currentColor];
-        if (currentColor === colours.length) {
-            currentColor = 0;
-        }
-
-        var blockStyleOl = new ol.style.Style({
-            stroke: new ol.style.Stroke({
-                color: colour,
-                width: 3
-            })
-        });
-
-        var block = angular.isDefined(feature.get('block')) ? feature.get('block') : feature;
-        if (block.deadhead) {
-            blockStyleOl = new ol.style.Style({
-                stroke: new ol.style.Stroke({
-                    color: "black",
-                    width: 3
-                })
-            });
-        }
-
-        if (blockStyles[block.id]) {
-            blockStyleOl = blockStyles[block.id];
-        } else {
-            blockStyles[block.id] = blockStyleOl;
-            currentColor++;
-        }
-
-        return blockStyleOl;
-    }
-
     function activeStyle(feature) {
 
         var type = angular.isDefined(feature.get) ? feature.get('type') : feature;
@@ -418,27 +234,8 @@ function MapStylesFactory() {
                 return accessLinkStyle;
             case "splitroute":
                 return splitRouteStyle;
-            case "group":
-                if (angular.isDefined(feature.get)) {
-                    return getGroupStyle(feature, false, feature.get('group'));
-                }
-                return null;
-            case "measure":
-                return measureStyle;
             case "deliverypoint":
                 return deliveryPointStyle;
-            case "collectionpoint":
-                return collectionPointStyle;
-            case "rmgpoint":
-                return rmgPointStyle;
-            case "routesim":
-                return blockStyle(feature);
-            case "routesim-point":
-                return routeSimPointStyle;
-            case "routeMaintenanceLine":
-                return getRouteMaintenanceStyle(feature);
-            case "routesim-person":
-                return routeSimPersonStyle;
             default:
                 return defaultStyle;
         }
@@ -447,18 +244,8 @@ function MapStylesFactory() {
     function inactiveStyle(feature) {
         var type = angular.isDefined(feature.get) ? feature.get('type') : feature;
         switch (type) {
-            case "measure":
-                return inactiveMeasureStyle;
-            case "collectionpoint":
             case "deliverypoint":
-            case "rmgpoint":
                 return inactivePointStyle;
-            case "routesim":
-                return blockStyle(feature);
-            case "routesim-point":
-                return inactiveRoutesimPoint;
-            case "routesim-person":
-                return inactiveRoutesimPerson;
             default:
                 return inactiveDefaultStyle;
         }
@@ -467,13 +254,7 @@ function MapStylesFactory() {
     function selectedStyle(feature) {
         var type = angular.isDefined(feature.get) ? feature.get('type') : feature;
         switch (type) {
-            case "group":
-                return getGroupStyle(feature, true);
-            case "measure":
-                return measureStyle
-            case "collectionpoint":
             case "deliverypoint":
-            case "rmgpoint":
                 return selectedPointStyle;
             case "accesslink":
                 return selectedLinkStyle;
@@ -484,43 +265,4 @@ function MapStylesFactory() {
         }
     }
 
-    function getGroupStyle(feature, selected, simGroup) {
-        var groupType = angular.isDefined(feature.get) ? feature.get('groupType') : feature;
-        if (simGroup) {
-            var point = angular.isDefined(feature.get) ? feature.get('point') : feature;
-            groupType = point.type;
-        }
-        if (selected) {
-            switch (groupType) {
-                case "Conversion":
-                    return selectedConversionStyle;
-                case "Tenement Block":
-                    return selectedTenementStyle;
-                case "Complex":
-                    return selectedComplexStyle;
-                case "Skyscraper":
-                    return selectedSkyscraperStyle
-                case "Service Point":
-                    return selectedServicePointStyle;
-                default:
-                    return defaultStyle;
-            }
-        }
-        else {
-            switch (groupType) {
-                case "Conversion":
-                    return conversionStyle;
-                case "Tenement Block":
-                    return tenementStyle;
-                case "Complex":
-                    return complexStyle;
-                case "Skyscraper":
-                    return skyscraperStyle
-                case "Service Point":
-                    return servicePointStyle;
-                default:
-                    return defaultStyle;
-            }
-        }
-    }
 }
