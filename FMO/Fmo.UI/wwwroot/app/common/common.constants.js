cc23e79d71cc582b008788a04caebde8428c0a3c
rictiwar <rictiwar@DIN19001008>
2017-05-19 18:06:27 +0530

    RFMO-1896 RouteLog hard coded values moved into constants

--- FMO/Fmo.UI/wwwroot/app/common/common.constants.js
+++ FMO/Fmo.UI/wwwroot/app/common/common.constants.js
@@ -10,7 +10,8 @@ GetSessionStorageItemType:"roleAccessData",
 EntityType: { DeliveryPoint: "DeliveryPoint", StreetNetwork: "StreetNetwork", Route: "Route", Postcode: "Postcode" },
 ButtonShapeType: { point: "point", line: "line", accesslink: "accesslink", select: "select" },
 GeometryType: { Point: "Point", LineString: "LineString" } ,
-pointTypes: { DeliveryPoint: { text: "Delivery Point", value: 'deliverypoint', style: "deliverypoint" }, AcessLink: { text: "Access Link", value: 'accesslink', style: "accesslink" }, Road: { text: "Road", value: 'roadlink', style: "roadlink" }, Selected: { text: "Selected", value: '', style: "deliverypoint" } }
+pointTypes: { DeliveryPoint: { text: "Delivery Point", value: 'deliverypoint', style: "deliverypoint" }, AcessLink: { text: "Access Link", value: 'accesslink', style: "accesslink" }, Road: { text: "Road", value: 'roadlink', style: "roadlink" }, Selected: { text: "Selected", value: '', style: "deliverypoint" } },
+RouteLogSelectionType: { Single: "Single", Multiple: "Multiple" }
 };
 
 angular.module('FMOApp')
