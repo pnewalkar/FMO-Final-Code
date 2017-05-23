659f8c0e10209a35a0c4750d74985be88b46f457
skashalk <swapna.kashalkar@capgemini.com>
2017-05-23 19:53:18 +0530

    RFMO-2382 service implemenation and nunit

---
+++ FMO/Fmo.UI/wwwroot/app/delivery-point/delivery-point-context.controller.js
@@ -0,0 +1,8 @@
+﻿angular.module('deliveryPoint')
+.controller('DeliveryPointContextController', ['$scope', '$state', '$stateParams', DeliveryPointContextController])
+function DeliveryPointContextController($scope, $state, $stateParams) {
+    debugger;
+    var vm = this;
+    vm.selectedDeliveryPoint = $stateParams.selectedDeliveryPoint;
+    vm.showDeliveryPoint = "";
+}
