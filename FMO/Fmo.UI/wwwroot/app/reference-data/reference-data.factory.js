9b97caf0dc1c10e352769366b4b591ff3f514a00
rictiwar <rictiwar@DIN19001008>
2017-05-23 15:19:09 +0530

    RFMO-2258 changes done

--- FMO/Fmo.UI/wwwroot/app/reference-data/reference-data.factory.js
+++ FMO/Fmo.UI/wwwroot/app/reference-data/reference-data.factory.js
@@ -8,7 +8,21 @@ function referencedataApiService($http, GlobalSettings) {
          referencedataApiService.getReferenceData = function () {
              return $http.get('./reference-data/ReferenceData.js');
 
-        };
+         };
+
+         function readJson() {
+             var deferred = $q.defer();
+
+             $http.get('./UI-string.json').success(function (response) {
+                 deferred.resolve(response);
+
+             }).error(function (err, status) {
+                 alert(err);
+                 deferred.reject(err);
+             });
+
+             return deferred.promise;
+         }
 
          return referencedataApiService;
 
