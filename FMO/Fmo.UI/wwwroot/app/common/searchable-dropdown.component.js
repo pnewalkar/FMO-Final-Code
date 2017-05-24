bc45c5c6daf0026130a7738b4ad8f63dd42fe989
CORP\jimali <jitendra.mali@capgemini.com>
2017-05-24 15:56:34 +0530

    RFMO-2214 Reusable component for searchable dropdown

---
+++ FMO/Fmo.UI/wwwroot/app/common/searchable-dropdown.component.js
@@ -0,0 +1,17 @@
+﻿angular.module('reusableComponent')
+  .component('customSearchableDropdown', {
+      bindings: {
+          labelText: "<",
+          searchTerm: "=",
+          isDeliveryRouteDisabled: "<",
+          selectedRoute: "=",
+          selectClass: "<",
+          selectHeaderClass: "<",
+          searchTerm: "=",
+          deliveryRoute: "<",
+          noResultFound: "<",
+          selectionChanged: "&"
+      },
+      templateUrl: './common/searchable-dropdown.template.html',
+      controller: 'CustomSearchableDropdownController as vm'
+  });
---
+++ FMO/Fmo.UI/wwwroot/app/common/searchable-dropdown.controller.js
@@ -0,0 +1,22 @@
+﻿angular
+     .module('reusableComponent')
+     .controller("CustomSearchableDropdownController", CustomSearchableDropdownController);
+function CustomSearchableDropdownController() {
+    var vm = this;
+    vm.clearSearchTerm = clearSearchTerm;
+    vm.selectionChange = selectionChange;
+    function clearSearchTerm() {
+        debugger;
+        vm.searchTerm = "";
+    }
+
+    function selectionChange() {
+        debugger;
+        var item = vm.selectedRoute;
+        vm.selectedRoute = item;
+       
+      //  vm.selectionChanged();
+        vm.selectionChanged({ selectedRoute: vm.selectedRoute });
+    }
+ 
+}
