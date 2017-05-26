41c2729f6b7f869edbd33eb79f2b9a05a367f752
rictiwar <rictiwar@DIN19001008>
2017-05-26 13:54:19 +0530

    add Error pop up files

---
+++ FMO/Fmo.UI/wwwroot/app/common/error.controller.js
@@ -0,0 +1,36 @@
+﻿angular
+    .module('error')
+    .controller("ErrorController", ErrorController);
+ErrorController.$inject = [
+     '$scope',
+     'message',
+     '$mdDialog','$rootScope'];
+
+function ErrorController(
+ $scope,
+ message,
+ $mdDialog,
+ $rootScope
+
+) {
+    var vm = this;
+    vm.openAlert = openAlert;
+    vm.message = message;
+    vm.closeWindow = closeWindow;
+ 
+    function closeWindow() {
+        $mdDialog.hide();
+    }
+
+    function openAlert() {
+        debugger;
+        var confirm = $mdDialog.confirm({
+            controller: 'ErrorController as vm',
+            templateUrl: './common/error-popup.html'
+            
+        })
+        $mdDialog.show(confirm).then(function () {
+            console.log(vm.message);
+        });
+    }
+};
