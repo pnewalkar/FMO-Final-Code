f1ae687014af8c44b5279af5041f309ed39905ce
Kashalkar, Swapna <swapna.kashalkar@capgemini.com>
2017-06-12 11:18:03 +0100

    RFMO-2199 added 5 new tests for Delivery route business service

--- RM/RM.Data.DeliveryRoute.WebAPI.Test/DeliveryRouteBusinessServiceFixture.cs
+++ RM/RM.Data.DeliveryRoute.WebAPI.Test/DeliveryRouteBusinessServiceFixture.cs
@@ -5,6 +5,7 @@ using Moq;
 using NUnit.Framework;
 using RM.CommonLibrary.EntityFramework.DataService.Interfaces;
 using RM.CommonLibrary.EntityFramework.DTO;
+using RM.CommonLibrary.EntityFramework.DTO.Model;
 using RM.CommonLibrary.HelperMiddleware;
 using RM.DataManagement.DeliveryRoute.WebAPI.BusinessService;
 using RM.DataManagement.DeliveryRoute.WebAPI.BusinessService.Implementation;
@@ -27,9 +28,11 @@ namespace RM.Data.DeliveryRoute.WebAPI.Test
         private Guid deliveryUnitID = System.Guid.NewGuid();
         private Guid operationalStateID = System.Guid.NewGuid();
         private Guid deliveryScenarioID = System.Guid.NewGuid();
+        private DeliveryRouteDTO deliveryRouteDto;
+        private List<ReferenceDataCategoryDTO> referenceDataCategoryDTOList;
 
         [Test]
-        public void TestFetchDeliveryScenario()
+        public void Test_FetchDeliveryScenario()
         {
             List<ScenarioDTO> expectedScenarioResult = testCandidate.FetchDeliveryScenario(operationalStateID, deliveryUnitID);
             Assert.NotNull(expectedScenarioResult);
@@ -38,7 +41,7 @@ namespace RM.Data.DeliveryRoute.WebAPI.Test
         }
 
         [Test]
-        public void TestFetchDeliveryRoute()
+        public void Test_FetchDeliveryRoute()
         {
             List<DeliveryRouteDTO> expectedDeliveryRouteResult = testCandidate.FetchDeliveryRoute(operationalStateID, deliveryScenarioID, deliveryUnitID);
             Assert.NotNull(expectedDeliveryRouteResult);
@@ -47,12 +50,56 @@ namespace RM.Data.DeliveryRoute.WebAPI.Test
         }
 
         [Test]
-        public void TestGetDeliveryRouteDetailsforPdfGeneration()
+        public void Test_GetDeliveryRouteDetailsforPdfGeneration()
         {
             var deliveryRouteGuid = System.Guid.Parse("B13D545D-2DE7-4E62-8DAD-00EC2B7FF8B8");
             var unitGuid = Guid.Parse("B51AA229-C984-4CA6-9C12-510187B81050");
             var expectedDeliveryRouteResult = testCandidate.GetDeliveryRouteDetailsforPdfGeneration(deliveryRouteGuid, unitGuid);
             Assert.NotNull(expectedDeliveryRouteResult);
+        }
+
+        [Test]
+        public void Test_FetchDeliveryRouteForBasicSearch()
+        {
+            mockDeliveryRouteDataService.Setup(n => n.FetchDeliveryRouteForBasicSearch(It.IsAny<string>(), It.IsAny<Guid>())).ReturnsAsync(new List<DeliveryRouteDTO>() { });
+            var expectedDeliveryRouteResult = testCandidate.FetchDeliveryRouteForBasicSearch("abc", Guid.NewGuid());
+            Assert.NotNull(expectedDeliveryRouteResult);
+        }
+
+        [Test]
+        public void Test_GetDeliveryRouteCount()
+        {
+            mockDeliveryRouteDataService.Setup(n => n.GetDeliveryRouteCount(It.IsAny<string>(), It.IsAny<Guid>())).ReturnsAsync(5);
+            var actualResult = testCandidate.GetDeliveryRouteCount("abc", Guid.NewGuid());
+            Assert.NotNull(actualResult);
+            Assert.AreEqual(actualResult.Result, 5);
+        }
+
+        [Test]
+        public void Test_FetchDeliveryRouteForAdvanceSearch()
+        {
+            mockDeliveryRouteDataService.Setup(n => n.FetchDeliveryRouteForAdvanceSearch(It.IsAny<string>(), It.IsAny<Guid>())).ReturnsAsync(new List<DeliveryRouteDTO>() { });
+            var actualResult = testCandidate.FetchDeliveryRouteForAdvanceSearch("abc", Guid.NewGuid());
+            Assert.NotNull(actualResult);
+        }
+
+        [Test]
+        public void Test_GenerateRouteLog()
+        {
+            mockDeliveryRouteIntegrationService.Setup(n => n.GetReferenceDataGuId(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(Guid.NewGuid());
+            mockDeliveryRouteDataService.Setup(n => n.GenerateRouteLog(It.IsAny<DeliveryRouteDTO>(), It.IsAny<Guid>(), It.IsAny<Guid>())).ReturnsAsync(new RouteLogSummaryModelDTO() { });
+            var expectedDeliveryRouteResult = testCandidate.GenerateRouteLog(deliveryRouteDto, Guid.NewGuid());
+            Assert.NotNull(actualDeliveryRouteResult);
+        }
+
+        [Test]
+        public void Test_CreateBlockSequenceForDeliveryPoint()
+        {
+            mockDeliveryRouteIntegrationService.Setup(x => x.GetReferenceDataSimpleLists(It.IsAny<List<string>>())).ReturnsAsync(referenceDataCategoryDTOList);
+            mockBlockSequenceDataService.Setup(n => n.AddBlockSequence(It.IsAny<BlockSequenceDTO>(), It.IsAny<Guid>())).ReturnsAsync(true);
+            var actualDeliveryRouteResult = testCandidate.CreateBlockSequenceForDeliveryPoint(Guid.NewGuid(), Guid.NewGuid());
+            Assert.NotNull(actualDeliveryRouteResult);
+            Assert.IsTrue(actualDeliveryRouteResult.Result);
         }
 
         protected override void OnSetup()
@@ -65,12 +112,24 @@ namespace RM.Data.DeliveryRoute.WebAPI.Test
             mockScenarioDataService = CreateMock<IScenarioDataService>();
             mockDeliveryRouteDataService = CreateMock<IDeliveryRouteDataService>();
 
-            List<ReferenceDataCategoryDTO> referenceDataCategoryDTOList = new List<ReferenceDataCategoryDTO>()
+            referenceDataCategoryDTOList = new List<ReferenceDataCategoryDTO>()
             {
                 new ReferenceDataCategoryDTO()
                 {
+                    CategoryName = ReferenceDataCategoryNames.OperationalObjectType,
+                    ReferenceDatas = new List<ReferenceDataDTO>()
+                    {
+                        new ReferenceDataDTO()
+                        {
+                            ReferenceDataName = null,
+                            ReferenceDataValue = ReferenceDataValues.OperationalObjectTypeDP,
+                            ID = Guid.Parse("4DBA7B39-D23E-493A-9B8F-B94D181A082F")
+                        }
+                    }
                 }
             };
+
+            deliveryRouteDto = new DeliveryRouteDTO() { };
 
             actualDeliveryRouteResult = new List<DeliveryRouteDTO>() { new DeliveryRouteDTO() { DeliveryRouteBarcode = "D0001", ID = Guid.NewGuid(), DeliveryScenario_Id = 1, ExternalId = 1, OperationalStatus_Id = 1, RouteMethodType_Id = 1, RouteName = "RouteOne", RouteNumber = "R004341" } };
             actualReferenceDataCategoryResult = new List<ReferenceDataDTO>() { new ReferenceDataDTO() { DataDescription = "Live", DisplayText = "Live", ReferenceDataName = "Live" } };
