9b5ed756c921b056fce174bf834a941ce1f32008
Kashalkar, Swapna <swapna.kashalkar@capgemini.com>
2017-06-12 15:19:27 +0100

    RFMO-2199 NUnit for NetworkManagerBusinessService

--- RM/RM.Data.NetworkManager.WebAPI.Test/NetworkManagerBusinessServiceFixture.cs
+++ RM/RM.Data.NetworkManager.WebAPI.Test/NetworkManagerBusinessServiceFixture.cs
@@ -9,6 +9,8 @@ using RM.DataManagement.NetworkManager.WebAPI.IntegrationService;
 using Moq;
 using RM.DataManagement.NetworkManager.WebAPI.BusinessService;
 using System.Data.Entity.Spatial;
+using RM.CommonLibrary.EntityFramework.DTO;
+using Microsoft.SqlServer.Types;
 
 namespace RM.Data.NetworkManager.WebAPI.Test
 {
@@ -21,6 +23,7 @@ namespace RM.Data.NetworkManager.WebAPI.Test
         private Mock<IRoadNameDataService> mockRoadNameDataService;
         private INetworkManagerBusinessService testCandidate;
         private DbGeometry dbGeometry;
+        private List<ReferenceDataCategoryDTO> referenceDataCategoryDTOList;
 
         [Test]
         public void Test_GetNearestNamedRoad()
@@ -36,9 +39,29 @@ namespace RM.Data.NetworkManager.WebAPI.Test
             mockRoadNameDataService = CreateMock<IRoadNameDataService>();
             mockStreetNetworkDataService = CreateMock<IStreetNetworkDataService>();
             dbGeometry = DbGeometry.PointFromText("POINT (488938 197021)", 27700);
+            SqlGeometry networkIntersectionPoint = SqlGeometry.Null;
+            NetworkLinkDTO networkLink = null;
+            Tuple < NetworkLinkDTO, SqlGeometry > tuple = new Tuple<NetworkLinkDTO, SqlGeometry>(networkLink, networkIntersectionPoint);
 
-         //   mockNetworkManagerIntegrationService.Setup()
+            referenceDataCategoryDTOList = new List<ReferenceDataCategoryDTO>()
+            {
+                new ReferenceDataCategoryDTO()
+                {
+                    ReferenceDatas = new List<ReferenceDataDTO>()
+                    {
+                        new ReferenceDataDTO()
+                        {
+                            ReferenceDataValue = FileType.Nyb.ToString(),
+                            ID = Guid.NewGuid(),
+                        }
+                    },
+                    CategoryName= Constants.PostalAddressType
+                }
+            };
 
+            mockNetworkManagerIntegrationService.Setup(x => x.GetReferenceDataSimpleLists(It.IsAny<List<string>>())).ReturnsAsync(referenceDataCategoryDTOList);
+
+            mockStreetNetworkDataService.Setup(x => x.GetNearestNamedRoad(It.IsAny<DbGeometry>(), It.IsAny<string>(), It.IsAny<List<ReferenceDataCategoryDTO>>())).Returns(tuple);
             testCandidate = new NetworkManagerBusinessService(mockStreetNetworkDataService.Object, mockNetworkManagerIntegrationService.Object, mockOsRoadLinkDataService.Object, mockRoadNameDataService.Object);
         }
     }
