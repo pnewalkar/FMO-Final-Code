67dbd766623edec9075be923c05700b7c60b68e9
Pandey, Abhinav <abhinav.a.pandey@capgemini.com>
2017-06-12 15:20:26 +0100

    RFMO-2207 unit test cases for map-toolbar controller & service, map-keys controller & service, map-panel controller and route-log controller

---
+++ RM/RM.Client/wwwroot/app/map-panel/tests/map-panel.controller.spec.js
@@ -0,0 +1,50 @@
+﻿describe('mapPanel:Controller', function () {
+
+    var $rootScope;
+    var $scope;
+    var $timeout;
+    var $interval;
+    var ctrl;
+
+    //load module
+    beforeEach(module('mapPanel'));
+
+    //get instance of module 
+    beforeEach(inject(function ($rootScope, $controller, _$timeout_) {
+        ctrl = $controller('MapPanelController', {
+            $timeout: _$timeout_,
+        });
+    }));
+
+
+    it('should be initialize mapView', function () {
+
+        //Need to create spy and call
+        spyOn(ctrl, 'initialize');
+
+        //cal to function
+        ctrl.initialize();
+
+        expect(ctrl.initialize).toHaveBeenCalled();
+        expect(ctrl.oncreate).toBeUndefined();
+
+        ctrl.oncreate = true;
+
+        expect(ctrl.oncreate).toBeDefined();
+        expect(ctrl.oncreate).not.toBeUndefined();
+        expect(ctrl.oncreate).toBe(true);
+
+    });
+
+    it('should be togglePanel collapsed if true', function () {
+
+        spyOn(ctrl, 'togglePanel');
+        ctrl.togglePanel();
+        expect(ctrl.collapsed).toBeUndefined();
+
+        //change to value true
+        ctrl.collapsed = true;
+        expect(ctrl.collapsed).toBe(true);
+    });
+
+});
