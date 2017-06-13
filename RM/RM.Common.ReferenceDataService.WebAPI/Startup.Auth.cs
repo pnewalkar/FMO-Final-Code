3cebbb5560caa51e12d28b5b53794bd5f2dc3266
Amol <amol.b.patil@capgemini.com>
2017-06-13 15:29:17 +0100

    RFMO-2181 - Move Authencation folder from web api to RM.CommonLibrary.Authentication

--- RM/RM.Common.ReferenceDataService.WebAPI/Startup.Auth.cs
+++ RM/RM.Common.ReferenceDataService.WebAPI/Startup.Auth.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.IdentityModel.Tokens;
-using RM.Common.ReferenceData.WebAPI.Authentication;
+using RM.CommonLibrary.Authentication;
 
 namespace RM.Common.ReferenceData.WebAPI
 {
