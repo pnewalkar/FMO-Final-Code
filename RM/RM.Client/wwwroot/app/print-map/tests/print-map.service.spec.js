1157a7352b8711301c128c7601938bc88bfda027
Pandey, Abhinav <abhinav.a.pandey@capgemini.com>
2017-06-21 16:59:55 +0100

    specs for print-map-service.spec.js

--- RM/RM.Client/wwwroot/app/print-map/tests/print-map.service.spec.js
+++ RM/RM.Client/wwwroot/app/print-map/tests/print-map.service.spec.js
@@ -1,30 +1,31 @@
 ﻿describe('Print Map: Service', function () {
-    //var rootScope;
-    //var scope;
-    //var $q;
-    //var $mdDialog
-    //var printMapService;
-
-    var mockPrintMapService, mockMdCancel;
-    //var mockPrintMapAPIService;
-
-    //var referencedataApiService;
-    //var referenceDataConstants;
-    //var CommonConstants;
+    var $q,
+        $mdDialog,
+        printMapAPIService,
+        CommonConstants,
+        referencedataApiService,
+        referenceDataConstants,
+        mockReferenceDataConstants
+        mapService
+        var DBCategoryName='dj';
 
     var generatePdfMockData = [{ "name": null, "value": "Live", "displayText": null, "description": "Live" }, { "id": "bee6048d-79b3-49a4-ad26-e4f5b988b7ab", "name": null, "value": "Not Live", "displayText": null, "description": "Not Live" }];
+    var mapPdfMockData = [{ "name": null, "value": "Live", "displayText": null, "description": "Live" }, { "id": "bee6048d-79b3-49a4-ad26-e4f5b988b7ab", "name": null, "value": "Not Live", "displayText": null, "description": "Not Live" }];
     var printMapMockData = [{ "name": null, "value": "Live", "displayText": null, "description": "Live" }, { "id": "bee6048d-79b3-49a4-ad26-e4f5b988b7ab", "name": null, "value": "Not Live", "displayText": null, "description": "Not Live" }];
+
+    var mockReferenceDataConstants = {
+    PDF_PageSize: { AppCategoryName: "PDF_PageSize", DBCategoryName: "PDF_PageSize", ReferenceDataNames: [{ AppReferenceDataName: "PDF_PageSize_A0", DBReferenceDataName: "PDF_PageSize_A0" }, { AppReferenceDataName: "PDF_PageSize_A1", DBReferenceDataName: "PDF_PageSize_A1" }, { AppReferenceDataName: "PDF_PageSize_A2", DBReferenceDataName: "PDF_PageSize_A2" }, { AppReferenceDataName: "PDF_PageSize_A3", DBReferenceDataName: "PDF_PageSize_A3" }, { AppReferenceDataName: "PDF_PageSize_A4", DBReferenceDataName: "PDF_PageSize_A4" }] }
+        };
 
     beforeEach(function () {
         module('printMap');
 
         module(function ($provide) {
-            mockMdCancel = jasmine.createSpy();
-            $provide.factory('$mdDialog', function () {
-                return function cancel() {
-                    return { cancel: mockMdCancel };
-                };
-            });
+            $provide.constant('referenceDataConstants', mockReferenceDataConstants);
+            MockMdCancel = jasmine.createSpy();
+                $provide.factory('$mdDialog', function() {
+                    return {cancel: MockMdCancel};
+                });
         });
       
         //inject with mockdata printMapAPIService
@@ -37,6 +38,12 @@
                     return deferred.promise;
                 }
 
+                function mapPdf(printMapDTO) {
+                    var deferred = $q.defer();
+                    deferred.resolve(mapPdfMockData);
+                    return deferred.promise;
+                }
+
                 function printMap(printMapDTO) {
                     var deferred = $q.defer();
                     deferred.resolve(printMapMockData);
@@ -45,6 +52,7 @@
 
                 return {
                     generatePdf: generatePdf,
+                    mapPdf: mapPdf,
                     printMap: printMap
                 };
             });
@@ -90,110 +98,90 @@
             });
         });
 
+        inject(function (_$rootScope_,_licensingInformationAccessorService_,_mapFactory_,_referenceDataConstants_,_mapService_,_printMapAPIService_,_CommonConstants_,_printMapService_,_$q_,_$mdDialog_,_referencedataApiService_) {
+            scope = _$rootScope_.$new(); //Generate new scope
+            $rootScope = _$rootScope_;
+            printMapService = _printMapService_;
+            printMapAPIService = _printMapAPIService_;
+            CommonConstants = _CommonConstants_;
+            $q = _$q_;
+            $mdDialog = _$mdDialog_;
+            referencedataApiService = _referencedataApiService_;
+            referenceDataConstants = _referenceDataConstants_;
+            mapService = _mapService_;
+            mapFactory = _mapFactory_;
+            licensingInformationAccessorService = _licensingInformationAccessorService_;
+        });
+
     });
 
-    beforeEach(function () {
-        inject(function ($injector) {
-            mockPrintMapService = $injector.get('printMapService');
-        });
+    it('should be window close when dialog button cancel ', function() {
+        spyOn(printMapService,'closeWindow').and.callThrough(); 
+
+        printMapService.closeWindow();
+
+        expect(printMapService.closeWindow).toHaveBeenCalled();
+        expect($mdDialog.cancel).toHaveBeenCalled();
+        expect($mdDialog.cancel).toHaveBeenCalledTimes(1);
+
     });
 
+    it('should initialize the function ', function() {
+        spyOn(printMapService,'initialize').and.callThrough();
+        printMapService.initialize();
+        
+        expect(printMapService.initialize).toHaveBeenCalled();
 
-   /* beforeEach(inject(function (
-        $service,
-        $q,
-        $mdDialog,
-        printMapAPIService,
-        CommonConstants,
-        referencedataApiService,
-        referenceDataConstants,
-        mapService,
-        $rootScope,
-        mapFactory,
-        $timeout,
-        licensingInformationAccessorService) {
-
-        mockmdDialog = $mdDialog;
-        mockPrintMapAPIService = printMapAPIService;
-        mockCommonConstants = CommonConstants;
-        mockReferencedataApiService = referencedataApiService;
-        mockReferenceDataConstants = referenceDataConstants;
-        mockMapService = mapService;
-        mockMapFactory = mapFactory;
-        mockLicensingInformationAccessorService = licensingInformationAccessorService;
-
-        mockPrintMapService = $service('printMapService', {
-            $q: $q,
-            $mdDialog: mockmdDialog,
-            printMapAPIService: mockPrintMapAPIService,
-            CommonConstants: mockCommonConstants,
-            referencedataApiService: mockReferencedataApiService,
-            referenceDataConstants: mockReferenceDataConstants,
-            mapService: mockMapService,
-            $rootScope: $rootScope,
-            mapFactory: mockMapFactory,
-            $timeout: $timeout,
-            licensingInformationAccessorService: mockLicensingInformationAccessorService
-        });
-    }));
-    */
-
-    describe('Should close the dialog-window', function () {
-        it('Should close window', function () {
-            mockPrintMapService.closeWindow();
-            expect(mockMdCancel).toHaveBeenCalled();
-            expect(mockMdCancel).toHaveBeenCalledTimes(1);
-        });
     });
 
-    //describe('Should initalize the service', function () {
-    //    it('Should initailize', function () {
-    //        spyOn(printMapService, 'initialize').and.callThrough();
-    //        mockPrintMapService.initialize();
-    //        expect(printMapService.initialize).not.toBeUndefined();
-    //        expect(printMapService.initialize).toHaveBeenCalled();
-    //        expect(printMapService.initialize).toHaveBeenCalledTimes(1);
-    //    });
-    //});
+    it('should be return promise response when call generatePdf', function (done) {
+       var pdfFilename = undefined;
+       printMapAPIService.generatePdf(pdfFilename).then(function (response) {
+           expect(response).toBeDefined();
+           expect(response).toBe(generatePdfMockData);
+           done();
+       });
+       scope.$digest();
+    });
 
-    //it('should be return promise response when call generatePdf', function (done) {
-    //    var pdfFilename = undefined;
-    //    printMapAPIService.generatePdf(pdfFilename).then(function (response) {
-    //        expect(response).toBeDefined();
-    //        expect(response).toBe(generatePdfMockData);
-    //        done();
-    //    });
-    //    scope.$digest();
-    //});
+    it('should be return promise response when call mapPdf', function (done) {
+       var printMapDto = undefined;
+       printMapAPIService.mapPdf(printMapDto).then(function (response) {
+           expect(response).toBeDefined();
+           expect(response).toBe(mapPdfMockData);
+           done();
+       });
+       scope.$digest();
+    });
 
-    //it('should be return promise response when call printMap', function (done) {
-    //    var printMapDTO = undefined;
-    //    printMapAPIService.printMap(printMapDTO).then(function (response) {
-    //        expect(response).toBeDefined();
-    //        expect(response).toBe(printMapMockData);
-    //        done();
-    //    });
-    //    scope.$digest();
-    //});
+    it('should be return promise response when call printMap', function (done) {
+       var printMapDTO = undefined;
+       printMapAPIService.printMap(printMapDTO).then(function (response) {
+           expect(response).toBeDefined();
+           expect(response).toBe(printMapMockData);
+           done();
+       });
+       scope.$digest();
+    });
 
-    //it('should be return promise response when call loadpdfsize', function (done) {
-    //    var referenceDataConstants = undefined;
-    //    referencedataApiService.getNameValueReferenceData(referenceDataConstants).then(function (response) {
-    //        expect(response).toBeDefined();
-    //        expect(response).toBe(printMapMockData);
-    //        done();
-    //    });
-    //    scope.$digest();
-    //});
+    it('should be return promise response when call loadpdfsize', function (done) {
+       var referenceDataConstants = undefined;
+       referencedataApiService.getNameValueReferenceData(referenceDataConstants).then(function (response) {
+           expect(response).toBeDefined();
+           expect(response).toBe(printMapMockData);
+           done();
+       });
+       scope.$digest();
+    });
 
-    //it('should be return promise response when call getReferencedata', function (done) {
-    //    var categoryName = undefined;
-    //    referencedataApiService.getNameValueReferenceData(categoryName).then(function (response) {
-    //        expect(response).toBeDefined();
-    //        expect(response).toBe(printMapMockData);
-    //        done();
-    //    });
-    //    scope.$digest();
-    //});
+    it('should be return promise response when call getReferencedata', function (done) {
+       var categoryName = undefined;
+       referencedataApiService.getNameValueReferenceData(categoryName).then(function (response) {
+           expect(response).toBeDefined();
+           expect(response).toBe(printMapMockData);
+           done();
+       });
+       scope.$digest();
+    });
 
 });
