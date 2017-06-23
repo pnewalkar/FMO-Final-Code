3d04e58bbf8a814a6d3d1a20027d658f1389cf28
Veerendra Poojary <veerendra.poojary@capgemini.com>
2017-06-23 09:19:47 +0100

    Fixed stye cop issues.

--- RM/RM.Operational.MapManager.WebAPI/Startup.MapExceptionTypes.cs
+++ RM/RM.Operational.MapManager.WebAPI/Startup.MapExceptionTypes.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Net;
 using Microsoft.AspNetCore.Builder;
-using RM.CommonLibrary.ExceptionMiddleware;
 using RM.CommonLibrary.ExceptionManagement.ExceptionHandling.ResponseExceptionHandler;
+using RM.CommonLibrary.ExceptionMiddleware;
 
 namespace RM.Operational.MapManager.WebAPI
 {
