cf9e55e03f1cc00567263ff9560730bfefce7f89
Veerendra Poojary <veerendra.poojary@capgemini.com>
2017-06-23 14:37:52 +0100

    RFMO-1670 map size issue fix.

--- RM/RM.Client/wwwroot/app/print-map/print-map.service.js
+++ RM/RM.Client/wwwroot/app/print-map/print-map.service.js
@@ -35,7 +35,6 @@ function printMapService(
     };
 
     function initialize() {
-
         var deferred = $q.defer();
 
         var result = { "MapDpi": null, "ImageHeight": null, "ImageWidth": null, "pdfSize": null };
@@ -89,7 +88,6 @@ function printMapService(
             angular.forEach(response, function (value, key) {
                 pdfSizeResult.push({ "DisplayText": value.description, "Value": value.value });
                 deferred.resolve(pdfSizeResult);
-
             });
         });
         return deferred.promise;
@@ -102,7 +100,6 @@ function printMapService(
             angular.forEach(response, function (value, key) {
                 refdataResult.push({ "Name": value.name, "Value": value.value });
                 deferred.resolve(refdataResult);
-
             });
         });
         return deferred.promise;
@@ -166,12 +163,9 @@ function printMapService(
         var imageHeightmm = getMapImageHeight(size, imageHeight);
         var mapWidth = getMapWidth(imageWidthmm, printMapDPI);
         var mapHeight = getMapHeight(imageHeightmm, printMapDPI);
-        mapService.setSize(mapWidth, mapHeight);
-        //After setting the map size it takes around 6-7 secs to load all the map tiles.Image should be captured after all the map tiles are loaded 
-        $timeout(captureImage, 7000, true, printOptions, resolution,map,scaleline);
-    }
-
-    function captureImage(printOptions, resolution,map,scaleline) {
+        var mapControl = document.getElementById('map');
+        mapControl.style.height = mapHeight + "px";
+        mapControl.style.width = mapWidth + "px";
         mapService.composeMap();
 
         var printMapDto = {
@@ -183,9 +177,10 @@ function printMapService(
             "EncodedString": $rootScope.canvas.toDataURL('image/png'),
             "License": licensingInformationAccessorService.getLicensingInformation()[0].value
         };
+
+        mapControl.removeAttribute('style');
         map.removeControl(scaleline);
         mapService.refreshLayers();
-        mapService.setOriginalSize();
         printMap(printMapDto).then(function (response) {
             mapPdf(response).then(function (response) {
                 displayPdf(response);
