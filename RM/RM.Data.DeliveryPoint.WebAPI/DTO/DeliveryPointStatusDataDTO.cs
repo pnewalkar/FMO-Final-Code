9c3a74be0ad9685516ba20cfcc45fe089deb1894
Crystal Gonsalves <crystal.gonsalves@capgemini.com>
2017-07-12 14:15:53 +0100

    RFMO-3566 Seperated DB entities Seperated TO Services modified for succesfull build status

---
+++ RM/RM.Data.DeliveryPoint.WebAPI/DTO/DeliveryPointStatusDataDTO.cs
@@ -0,0 +1,20 @@
+namespace RM.Data.DeliveryPoint.WebAPI.DTO
+{
+    using System;
+    public class DeliveryPointStatusDataDTO
+    {
+        public Guid ID { get; set; }
+
+        public Guid LocationGUID { get; set; }
+
+        public Guid OperationalStatusGUID { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime? EndDateTime { get; set; }
+
+        public DateTime RowCreateDateTime { get; set; }
+
+        public DeliveryPointDataDTO DeliveryPoint { get; set; }
+    }
+}
