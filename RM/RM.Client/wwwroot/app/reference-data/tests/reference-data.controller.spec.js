466ec5f6a83161c650d2cbe7a313ca48853eb52e
Pandey, Abhinav <abhinav.a.pandey@capgemini.com>
2017-07-17 07:26:42 +0100

    Jasmine for reference data module

---
+++ RM/RM.Client/wwwroot/app/reference-data/tests/reference-data.controller.spec.js
@@ -0,0 +1,59 @@
+﻿'use strict';
+describe('Reference data: Controller', function () {
+	var $scope;
+	var $filter;
+    var $rootScope;
+	var referencedataApiService;
+	var referenceDataConstants;
+
+	beforeEach(function () {
+		module('referencedata'); 
+        module(function ($provide) {});
+		$provide.factory('referencedataApiService', function($q){
+			function initialize() {};
+
+			function referenceData() {
+                    deferred = $q.defer();
+                    return deferred.promise;
+                };
+
+			return{
+				initialize: initialize,
+				referenceData: referenceData
+			}
+		});
+	});        
+
+	inject(function (_$controller_,_$rootScope_,_referencedataApiService_,_referenceDataConstants_,_$filter_){
+			$rootScope = _$rootScope_;
+            $scope = $rootScope.$new();
+            referencedataApiService = _referencedataApiService_;
+            referenceDataConstants = _referenceDataConstants_;
+            $filter = _$filter_;
+
+            vm = _$controller_('ReferenceDataController', {
+                $scope : $scope,
+                referencedataApiService : referencedataApiService,
+                referenceDataConstants : referenceDataConstants,
+                $filter : $filter
+            });
+
+            spyOn($scope, '$emit').and.callThrough();
+            spyOn($scope, '$on').and.callThrough();
+            $scope.$emit.and.stub();
+        });
+
+	it('should promise to return a success response once referenceData method is called', function() {
+        var deferredSuccess = $q.defer(); 
+        var response = {};
+        spyOn(referencedataApiService, 'referenceData').and.returnValue(deferredSuccess.promise);
+        
+        vm.referenceData();
+        
+        deferredSuccess.resolve(response);
+        $scope.$digest();
+
+        expect(referencedataApiService.referenceData).toHaveBeenCalled();
+    });
+
+    });
---
+++ RM/RM.Client/wwwroot/app/reference-data/tests/reference-data.factory.spec.js
@@ -0,0 +1,87 @@
+﻿'use strict';
+describe('Reference Data : Factory', function() {
+	var $q;
+    var defrred;
+    var $rootScope;
+    var $httpBackend;
+    var expectedUrl;
+    var GlobalSettings;
+    var referencedataApiService;
+
+    var MockGlobalSettings = {
+        apiUrl: undefined
+    };
+
+    beforeEach(function(){
+        module('referencedata');
+        module(function ($provide) {
+            $provide.value('GlobalSettings', MockGlobalSettings);
+        });
+        inject(function (_$rootScope_,_referencedataApiService_,_$httpBackend_, _$q_,_GlobalSettings_) {
+            referencedataApiService = _referencedataApiService_;
+            $httpBackend = _$httpBackend_;
+            $q = _$q_;
+            GlobalSettings = _GlobalSettings_;
+            $rootScope: _$rootScope_;
+        });
+    });
+
+    it('should promise to return a success response once getReferenceData method is called', function() {
+        expect(getReferenceData).toHaveBeenCalled();
+    });
+
+    it('should promise to return a success response once readJson method is called', function() {
+        var response;
+        var expectedUrl = GlobalSettings.apiUrl+'readJsonData';
+        var getReferenceDataMockData = [{}]; 
+
+        $httpBackend.when('GET', expectedUrl)
+            .respond(200, getReferenceDataMockData);
+
+        referencedataApiService.readJson('readJsonData')
+            .then(function(data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(readJsonMockData);
+    });
+
+    it('should promise to return a success response once getSimpleListsReferenceData method is called', function() {
+        var response;
+        var expectedUrl = GlobalSettings.apiUrl+'/ReferenceDataManager/simpleLists?listName=';
+        var getSimpleListsReferenceDataMockData = [{}]; 
+
+        $httpBackend.when('GET', expectedUrl)
+            .respond(200, getSimpleListsReferenceDataMockData);
+
+        referencedataApiService.getSimpleListsReferenceData('listName')
+            .then(function(data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(getSimpleListsReferenceDataMockData);
+    });
+
+    it('should promise to return a success response once getNameValueReferenceData method is called', function() {
+        var response;
+        var expectedUrl = GlobalSettings.apiUrl+'/ReferenceDataManager/nameValuePairs?appGroupName=';
+        var getNameValueReferenceDataMockData = [{}]; 
+
+        $httpBackend.when('GET', expectedUrl)
+            .respond(200, getNameValueReferenceDataMockData);
+
+        referencedataApiService.getNameValueReferenceData('appGroupName')
+            .then(function(data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(getNameValueReferenceDataMockData);
+    });
+
+});
