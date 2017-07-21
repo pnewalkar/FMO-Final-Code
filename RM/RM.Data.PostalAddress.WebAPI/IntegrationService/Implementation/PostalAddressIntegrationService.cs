8388829a6e6dd0ebaf1ee3a4815a48bf002a25f7
Rahul Upadhyay <rahul.upadhyay@capgemini.com>
2017-07-21 09:35:51 +0100

    RFMO - 3534 - AddressLocation Changes

--- RM/RM.Data.PostalAddress.WebAPI/IntegrationService/Implementation/PostalAddressIntegrationService.cs
+++ RM/RM.Data.PostalAddress.WebAPI/IntegrationService/Implementation/PostalAddressIntegrationService.cs
@@ -14,6 +14,7 @@ using RM.CommonLibrary.ExceptionMiddleware;
 using RM.CommonLibrary.HelperMiddleware;
 using RM.CommonLibrary.Interfaces;
 using RM.CommonLibrary.LoggingMiddleware;
+using RM.CommonLibrary.Utilities.HelperMiddleware.GeoJsonData;
 using RM.Data.PostalAddress.WebAPI.Utils;
 using RM.DataManagement.PostalAddress.WebAPI.DTO;
 using RM.DataManagement.PostalAddress.WebAPI.IntegrationService.Interface;
@@ -468,7 +469,9 @@ namespace RM.DataManagement.PostalAddress.WebAPI.IntegrationService.Implementati
                     throw new ServiceException(responseContent);
                 }
 
-                DbGeometry approxLocation = JsonConvert.DeserializeObject<DbGeometry>(result.Content.ReadAsStringAsync().Result);
+                // DbGeometry approxLocation = JsonConvert.DeserializeObject<DbGeometry>(result.Content.ReadAsStringAsync().Result);
+                DBGeometryObj locationObject = JsonConvert.DeserializeObject<DBGeometryObj>(result.Content.ReadAsStringAsync().Result);
+                DbGeometry approxLocation = locationObject.dbGeometry;
                 loggingHelper.LogMethodExit(methodName, priority, exitEventId);
 
                 return approxLocation;
