7b555e31a618b4f3a289788e7c004f46ed81886f
Kashalkar, Swapna <swapna.kashalkar@capgemini.com>
2017-07-24 11:47:18 +0100

    RFMO-3567 nunit for streetnetwork data service

--- RM/RM.Data.NetworkManager.WebAPI.Test/DataService/StreetNetworkDataServiceFixture.cs
+++ RM/RM.Data.NetworkManager.WebAPI.Test/DataService/StreetNetworkDataServiceFixture.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using RM.CommonLibrary.ConfigurationMiddleware;
 using RM.CommonLibrary.DataMiddleware;
+using RM.CommonLibrary.EntityFramework.DTO;
 using RM.CommonLibrary.HelperMiddleware;
 using RM.CommonLibrary.LoggingMiddleware;
 using RM.DataManagement.NetworkManager.WebAPI.DataService.Implementation;
@@ -21,98 +23,102 @@ namespace RM.Data.NetworkManager.WebAPI.Test.DataService
         private Mock<IDatabaseFactory<NetworkDBContext>> mockDatabaseFactory;
         private IStreetNetworkDataService testCandidate;
         private Mock<ILoggingHelper> mockILoggingHelper;
+        private Mock<IConfigurationHelper> mockIConfigurationHelper;
         private Guid deliveryUnitID = System.Guid.NewGuid();
         private Guid unit1Guid;
-        private Guid unit2Guid;
-        private Guid unit3Guid;
-        private Guid user1Id;
-        private Guid user2Id;
         private DbGeometry accessLinkLine;
-        /*
+        private DbGeometry point;
+        private List<ReferenceDataCategoryDTO> referenceDataCategoryList;
 
         [Test]
-        public async Task TestFetchStreetNamesForBasicSearchValid()
+        public async Task Test_GetStreetNamesForAdvanceSearch()
         {
-            var actualResult = await testCandidate.FetchStreetNamesForBasicSearch("Test", unit1Guid);
+            var actualResult = await testCandidate.GetStreetNamesForAdvanceSearch("Test", unit1Guid);
+            Assert.IsNotNull(actualResult);
+            Assert.IsTrue(actualResult.Count == 7);
+        }
+
+        [Test]
+        public async Task Test_GetStreetNamesForBasicSearch()
+        {
+            var actualResult = await testCandidate.GetStreetNamesForBasicSearch("Test", unit1Guid);
             Assert.IsNotNull(actualResult);
             Assert.IsTrue(actualResult.Count == 5);
         }
 
         [Test]
-        public async Task TestFetchStreetNamesForBasicSearchInvalid()
+        public async Task Test_GetStreetNameCounth()
         {
-            var actualResult = await testCandidate.FetchStreetNamesForBasicSearch("invalid_Test", unit1Guid);
+            var actualResult = await testCandidate.GetStreetNameCount("Test", unit1Guid);
             Assert.IsNotNull(actualResult);
-            Assert.IsTrue(actualResult.Count == 0);
+            Assert.IsTrue(actualResult == 7);
         }
 
         [Test]
-        public async Task TestFetchStreetNamesForBasicSearchNull()
+        public void Test_GetNearestNamedRoad()
         {
-            var actualResult = await testCandidate.FetchStreetNamesForBasicSearch(null, unit1Guid);
+            var actualResult = testCandidate.GetNearestNamedRoad(point, "XYZ1", referenceDataCategoryList);
             Assert.IsNotNull(actualResult);
-            Assert.IsTrue(actualResult.Count == 5);
+            Assert.IsNotNull(actualResult.Item1);
+            Assert.IsNotNull(actualResult.Item2);
         }
 
         [Test]
-        public async Task TestGetStreetNameCountValid()
+        public void Test_GetNearestSegment()
         {
-            var actualResultCount = await testCandidate.GetStreetNameCount("Test", unit1Guid);
-            Assert.IsNotNull(actualResultCount);
-            Assert.IsTrue(actualResultCount == 7);
+            var actualResult = testCandidate.GetNearestSegment(point, referenceDataCategoryList);
+            Assert.IsNotNull(actualResult);
+            Assert.IsNotNull(actualResult.Item1);
+            Assert.IsNotNull(actualResult.Item2);
         }
 
         [Test]
-        public async Task TestGetStreetNameCountInvalid()
+        public void Test_GetNetworkLink()
         {
-            var actualResultCount = await testCandidate.GetStreetNameCount("invalid_Test", unit1Guid);
-            Assert.IsNotNull(actualResultCount);
-            Assert.IsTrue(actualResultCount == 0);
+            var actualResult = testCandidate.GetNetworkLink(new Guid("8134AA41-391F-4579-A18D-D7EDF5B5F911"));
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(actualResult.ID, new Guid("8134AA41-391F-4579-A18D-D7EDF5B5F911"));
         }
 
         [Test]
-        public async Task TestGetStreetNameCountNull()
-        {
-            var actualResultCount = await testCandidate.GetStreetNameCount(null, unit1Guid);
-            Assert.IsNotNull(actualResultCount);
-            Assert.IsTrue(actualResultCount == 7);
-        }
-
-        [Test]
-        public void TestGetCrossingNetworkLink()
+        public void Test_GetCrossingNetworkLink()
         {
             string coordinates = "POLYGON((511570.8590967182 106965.35195621933, 511570.8590967182 107474.95297542136, 512474.1409032818 107474.95297542136, 512474.1409032818 106965.35195621933, 511570.8590967182 106965.35195621933))";
-            var result = testCandidate.GetCrossingNetworkLink(coordinates, accessLinkLine);
-            Assert.IsNotNull(result);
+            var actualResult = testCandidate.GetCrossingNetworkLink(coordinates, accessLinkLine);
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(actualResult.Count, 0);
         }
-        */
 
         protected override void OnSetup()
         {
-            unit1Guid = Guid.NewGuid();
-            unit2Guid = Guid.NewGuid();
-            unit3Guid = Guid.NewGuid();
-            user1Id = System.Guid.NewGuid();
-            user2Id = System.Guid.NewGuid();
+            unit1Guid = new Guid("8534AA41-391F-4579-A18D-D7EDF5B5F918");
+            mockIConfigurationHelper = CreateMock<IConfigurationHelper>();
+
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+
             var unitBoundary = DbGeometry.PolygonFromText("POLYGON ((505058.162109375 100281.69677734375, 518986.84887695312 100281.69677734375, 518986.84887695312 114158.546875, 505058.162109375 114158.546875, 505058.162109375 100281.69677734375))", 27700);
             accessLinkLine = DbGeometry.LineFromText("LINESTRING (488938 197021, 488929.9088937093 197036.37310195228)", 27700);
+            point = DbGeometry.PointFromText("POINT (488938 197021)", 27700);
 
             var streetNamesList = new List<StreetName>()
             {
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad1", DesignatedName = "XYZ1", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad2", DesignatedName = "XYZ2", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad3", DesignatedName = "XYZ3", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad4", DesignatedName = "XYZ4", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad5", DesignatedName = "XYZ5", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad6", DesignatedName = "XYZ6", Geometry = unitBoundary },
-                new StreetName() { ID = Guid.NewGuid(), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad7", DesignatedName = "XYZ7", Geometry = unitBoundary }
+                new StreetName() { ID = new Guid("8134AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad1", DesignatedName = "XYZ1", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8234AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad2", DesignatedName = "XYZ2", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8334AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad3", DesignatedName = "XYZ3", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8434AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad4", DesignatedName = "XYZ4", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8534AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad5", DesignatedName = "XYZ5", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8634AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad6", DesignatedName = "XYZ6", Geometry = unitBoundary },
+                new StreetName() { ID = new Guid("8734AA41-391F-4579-A18D-D7EDF5B5F918"), StreetType = "t1", Descriptor = "d1", NationalRoadCode = "Testroad7", DesignatedName = "XYZ7", Geometry = unitBoundary }
             };
 
             var networkLinkList = new List<NetworkLink>()
             {
                 new NetworkLink()
                 {
-                    LinkGeometry = accessLinkLine
+                    LinkGeometry = accessLinkLine,
+                    StreetNameGUID = new Guid("8134AA41-391F-4579-A18D-D7EDF5B5F918"),
+                    NetworkLinkTypeGUID = Guid.Empty,
+                    ID = new Guid("8134AA41-391F-4579-A18D-D7EDF5B5F911")
                 }
             };
 
@@ -120,11 +126,56 @@ namespace RM.Data.NetworkManager.WebAPI.Test.DataService
             {
                 new Location()
                 {
-                    ID = new Guid("8534AA41-391F-4579-A18D-D7EDF5B5F918")
+                    ID = unit1Guid,
+                    Shape = unitBoundary
+                }
+            };
+
+            referenceDataCategoryList = new List<ReferenceDataCategoryDTO>()
+            {
+                new ReferenceDataCategoryDTO()
+                {
+                    CategoryName = ReferenceDataCategoryNames.NetworkLinkType,
+                    ReferenceDatas = new List<ReferenceDataDTO>()
+                    {
+                        new ReferenceDataDTO()
+                        {
+                            ReferenceDataValue = ReferenceDataValues.NetworkLinkPathLink
+                        }
+                    },
+                    Id = new Guid("1534AA41-391F-4579-A18D-D7EDF5B5F918")
+                },
+
+                new ReferenceDataCategoryDTO()
+                {
+                    CategoryName = ReferenceDataCategoryNames.NetworkLinkType,
+                    ReferenceDatas = new List<ReferenceDataDTO>()
+                    {
+                        new ReferenceDataDTO()
+                        {
+                            ReferenceDataValue = ReferenceDataValues.NetworkLinkRoadLink
+                        }
+                    },
+                    Id = new Guid("2534AA41-391F-4579-A18D-D7EDF5B5F918")
+                },
+
+                new ReferenceDataCategoryDTO()
+                {
+                    CategoryName = ReferenceDataCategoryNames.AccessLinkParameters,
+                    ReferenceDatas = new List<ReferenceDataDTO>()
+                    {
+                        new ReferenceDataDTO()
+                        {
+                            ReferenceDataName =  ReferenceDataValues.AccessLinkDiffRoadMaxDistance,
+                            ReferenceDataValue = "5"
+                        }
+                    },
+                    Id = new Guid("3534AA41-391F-4579-A18D-D7EDF5B5F918")
                 }
             };
 
             mockNetworkDBContext = CreateMock<NetworkDBContext>();
+            mockILoggingHelper = CreateMock<ILoggingHelper>();
 
             var mockAsynEnumerable = new DbAsyncEnumerable<StreetName>(streetNamesList);
             var mockStreetName = MockDbSet(streetNamesList);
@@ -145,7 +196,6 @@ namespace RM.Data.NetworkManager.WebAPI.Test.DataService
             mockNetworkDBContext.Setup(x => x.Set<Location>()).Returns(mockLocation.Object);
             mockNetworkDBContext.Setup(x => x.Locations).Returns(mockLocation.Object);
             mockLocation.Setup(x => x.AsNoTracking()).Returns(mockLocation.Object);
-                
 
             var mockAsynEnumerable3 = new DbAsyncEnumerable<NetworkLink>(networkLinkList);
             var mockNetworkLink = MockDbSet(networkLinkList);
@@ -161,9 +211,10 @@ namespace RM.Data.NetworkManager.WebAPI.Test.DataService
             rmTraceManagerMock.Setup(x => x.StartTrace(It.IsAny<string>(), It.IsAny<Guid>()));
             mockILoggingHelper.Setup(x => x.RMTraceManager).Returns(rmTraceManagerMock.Object);
 
+            mockIConfigurationHelper.Setup(x => x.ReadAppSettingsConfigurationValues(It.IsAny<string>())).Returns("5");
             mockDatabaseFactory = CreateMock<IDatabaseFactory<NetworkDBContext>>();
             mockDatabaseFactory.Setup(x => x.Get()).Returns(mockNetworkDBContext.Object);
-            testCandidate = new StreetNetworkDataService(mockDatabaseFactory.Object, mockILoggingHelper.Object);
+            testCandidate = new StreetNetworkDataService(mockDatabaseFactory.Object, mockILoggingHelper.Object, mockIConfigurationHelper.Object);
         }
     }
 }
