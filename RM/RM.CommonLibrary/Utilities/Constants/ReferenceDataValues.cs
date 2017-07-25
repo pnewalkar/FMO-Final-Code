674e4d00261ba7f0a0202f4f86452d4cb41c8fe3
Amol <amol.b.patil@capgemini.com>
2017-07-25 14:40:17 +0100

    RFMO-3566 - Reference Data constant fix

--- RM/RM.CommonLibrary/Utilities/Constants/ReferenceDataValues.cs
+++ RM/RM.CommonLibrary/Utilities/Constants/ReferenceDataValues.cs
@@ -8,7 +8,7 @@
         public const string PavementWidth = "Pavement Width";
         public const string PropertyDepth = "Property Depth";
         public const string Residential = "Residential";
-        public const string Organisation = "Commercial";
+        public const string Organisation = "Organisation";
         public const string ResidentialRoadWidthMultiplicationFactor = "Residential - Road Width Multiplication Factor";
         public const string ResidentialPavementWidthMultiplicationFactor = "Residential - Pavement Width Multiplication Factor";
         public const string ResidentialHouseDepthMultiplicationFactor = "Residential - House Depth Multiplication Factor";
