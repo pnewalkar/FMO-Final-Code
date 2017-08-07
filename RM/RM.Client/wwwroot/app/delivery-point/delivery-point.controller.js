899a36a693fb4faa461c1fd45a9b4612e9f64686
Kashalkar, Swapna <swapna.kashalkar@capgemini.com>
2017-08-07 12:40:23 +0100

    RFMO-4091 fixed

--- RM/RM.Client/wwwroot/app/delivery-point/delivery-point.controller.js
+++ RM/RM.Client/wwwroot/app/delivery-point/delivery-point.controller.js
@@ -63,6 +63,7 @@ function DeliveryPointController(
     vm.initialize = initialize;
     vm.setRangeValidation = setRangeValidation;
     vm.createDeliveryPointsRange = createDeliveryPointsRange;
+    vm.setDeliveryPointUseIndicator = setDeliveryPointUseIndicator;
 
     vm.positionedThirdPartyDeliveryPointList = $stateParams.positionedThirdPartyDeliveryPointList;
     vm.positionedDeliveryPointList = $stateParams.positionedDeliveryPointList;
@@ -187,10 +188,12 @@ function DeliveryPointController(
                 vm.selectedDPUse = response.selectedDPUse;
             });
         }
-        else {
-            vm.dpUse = vm.dpUseType;
-            vm.selectedDPUse = "";
-        }
+    }
+
+    function setDeliveryPointUseIndicator()
+    {
+        vm.dpUse = vm.dpUseType;
+        vm.selectedDPUse = "";
     }
 
     function toggle(item) {
