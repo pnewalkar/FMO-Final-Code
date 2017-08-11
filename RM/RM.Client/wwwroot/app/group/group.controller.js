68e50e691c9e886ffa0557306bfa9bba77e25bb0
Bhosale, Supriya Suresh <supriya-suresh.bhosale@capgemini.com>
2017-08-11 15:52:21 +0100

    RFMO-139 Replaced 'Area' with 'Group' in map toolbar

--- RM/RM.Client/wwwroot/app/group/group.controller.js
+++ RM/RM.Client/wwwroot/app/group/group.controller.js
@@ -3,11 +3,20 @@
     .controller("GroupController", GroupController);
 
 GroupController.$inject = [
+    'mapToolbarService',
+    '$rootScope'
 ];
 
 function GroupController(
+    mapToolbarService,
+    $rootScope
    ) {
 
     var vm = this;
+    vm.addGroup = addGroup;
+
+    function addGroup() {
+        $rootScope.$emit('resetMapToolbar', { "isGroupAction": true });
+    }
 }
 
