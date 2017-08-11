fcd5d1a295541a981a7585d8338c7c3da089dc74
rictiwar <richa.a.tiwari@capgemini.com>
2017-08-11 12:08:32 +0100

    RFMO-184 UI Changes Committed

--- RM/RM.Client/wwwroot/app/delivery-point/delivery-point-context.controller.js
+++ RM/RM.Client/wwwroot/app/delivery-point/delivery-point-context.controller.js
@@ -5,13 +5,14 @@
 
 DeliveryPointContextController.$inject = ['GlobalSettings',
     '$rootScope',
- 'selectedDeliveryPointService','CommonConstants']
+ 'selectedDeliveryPointService', 'CommonConstants', 'popUpSettingService', '$mdDialog']
 
 function DeliveryPointContextController(GlobalSettings,
     $rootScope,
-selectedDeliveryPointService, CommonConstants)
+selectedDeliveryPointService, CommonConstants, popUpSettingService, $mdDialog)
 {
-        var vm = this;
+    var vm = this;
+    vm.deleteDeliveryPoint = deleteDeliveryPoint;
         vm.selectedDeliveryPoint = selectedDeliveryPointService.getSelectedDeliveryPoint();
         if (vm.selectedDeliveryPoint != null) {
             vm.selectedDeliveryPointType = vm.selectedDeliveryPoint.type;
@@ -22,5 +23,22 @@ selectedDeliveryPointService, CommonConstants)
             $rootScope.$broadcast('showDeliveryPointDetails', {
                 contextTitle: CommonConstants.DetailsOfDeliveryPoint,
                 featureType: vm.selectedDeliveryPointType
-            });      
+            });
+
+            function deleteDeliveryPoint() {
+                
+                var deleteDeliveryPointTemplate = popUpSettingService.deleteDeliveryPoint();
+                openModalPopup(deleteDeliveryPointTemplate);
+                
+            }
+
+            function openModalPopup(modalSetting) {
+                var popupSetting = modalSetting;
+                $mdDialog.show(popupSetting).then(function (returnedData) {
+                });
+
+
+            }
+
+
     }
