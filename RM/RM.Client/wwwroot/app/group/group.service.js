8416518a499b4bf662e05b21b657a591eaeedaee
Bhosale, Supriya Suresh <supriya-suresh.bhosale@capgemini.com>
2017-08-16 09:21:58 +0100

    RFMO-139 Fetching delivery point group types from ref data

--- RM/RM.Client/wwwroot/app/group/group.service.js
+++ RM/RM.Client/wwwroot/app/group/group.service.js
@@ -1,14 +1,46 @@
 'use strict';
 angular.module('group')
     .factory('groupService', groupService);
-groupService.$inject = [];
+groupService.$inject = [
+    'referencedataApiService',
+    '$q'];
 
-function groupService() {
+function groupService(
+    referencedataApiService,
+    $q
+    ) {
 	return {
 		initialize: initialize
 	}
 
 	function initialize() {
-	
+	    var deferred = $q.defer();
+
+	    var result = { "DeliveryGroupType": null, "ServicePointType": null };
+	    $q.all([
+            DeliveryGroupTypes(),
+            ServicePointTypes()
+	    ]).then(function (response) {
+	        result.DeliveryGroupType = response[0];
+	        result.ServicePointType = response[1];
+	        deferred.resolve(result)
+	    })
+	    return deferred.promise;
+	}
+
+	function DeliveryGroupTypes() {
+	    var deferred = $q.defer();
+	    referencedataApiService.getSimpleListsReferenceData(referenceDataConstants.DeliveryGroupType.DBCategoryName).then(function (response) {
+	        deferred.resolve(response.listItems);
+	    });
+	    return deferred.promise;
+	}
+
+	function ServicePointTypes() {
+	    var deferred = $q.defer();
+	    referencedataApiService.getSimpleListsReferenceData(referenceDataConstants.ServicePointType.DBCategoryName).then(function (response) {
+	        deferred.resolve(response.listItems);
+	    });
+	    return deferred.promise;
 	}
 }
