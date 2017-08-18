fe5777ef8f3d3aa54d313134411a52e66c75bdfe
Ashokan, Akash <akash.ashokan@capgemini.com>
2017-08-18 12:03:24 +0100

    RFMO-138 Method stubs to fetch groups

--- RM/RM.Data.DeliveryPointGroupManager.WebAPI/Controllers/DeliveryPointGroupController.cs
+++ RM/RM.Data.DeliveryPointGroupManager.WebAPI/Controllers/DeliveryPointGroupController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RM.CommonLibrary.HelperMiddleware;
 using RM.CommonLibrary.LoggingMiddleware;
 using RM.DataManagement.DeliveryPointGroupManager.WebAPI.BusinessService;
@@ -10,7 +11,7 @@ namespace RM.DataManagement.DeliveryPointGroupManager.WebAPI.Controllers
     {
         #region Member Variables
 
-        private IDeliveryPointGroupBusinessService businessService = default(IDeliveryPointGroupBusinessService);
+        private IDeliveryPointGroupBusinessService deliveryPointGroupBusinessService = default(IDeliveryPointGroupBusinessService);
         private ILoggingHelper loggingHelper = default(ILoggingHelper);
 
         private int priority = LoggerTraceConstants.DeliveryPointGroupManagerAPIPriority;
@@ -21,12 +22,37 @@ namespace RM.DataManagement.DeliveryPointGroupManager.WebAPI.Controllers
 
         #region Constructors
 
-        public DeliveryPointGroupController(IDeliveryPointGroupBusinessService businessService, ILoggingHelper loggingHelper)
+        public DeliveryPointGroupController(IDeliveryPointGroupBusinessService deliveryPointGroupBusinessService, ILoggingHelper loggingHelper)
         {
-            this.businessService = businessService;
+            this.deliveryPointGroupBusinessService = deliveryPointGroupBusinessService;
             this.loggingHelper = loggingHelper;
         }
 
         #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// This method is used to fetch Delivery Point Groups.
+        /// </summary>
+        /// <param name="boundaryBox">boundaryBox as string</param>
+        /// <returns>GeoJson string of Delivery Point Groups</returns>
+        [Authorize]
+        [HttpGet("DeliveryPointGroups")]
+        public IActionResult GetDeliveryPointGroups(string bbox)
+        {
+            using (loggingHelper.RMTraceManager.StartTrace("WebService.GetDeliveryPointGroups"))
+            {
+                string methodName = typeof(DeliveryPointGroupController) + "." + nameof(GetDeliveryPointGroups);
+                loggingHelper.LogMethodEntry(methodName, priority, entryEventId);
+
+                string deliveryGroups = deliveryPointGroupBusinessService.GetDeliveryPointGroups(bbox, this.CurrentUserUnit);
+
+                loggingHelper.LogMethodExit(methodName, priority, exitEventId);
+                return Ok(deliveryGroups);
+            }
+        }
+
+        #endregion
     }
 }
