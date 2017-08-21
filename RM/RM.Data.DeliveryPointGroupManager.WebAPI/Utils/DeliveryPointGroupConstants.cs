3f245ce12856b9ff24b28c6d60266c56a26abbd4
Rahul Upadhyay <rahul.upadhyay@capgemini.com>
2017-08-21 16:46:31 +0100

    RFMO-139 - Converted to DBGeometry

--- RM/RM.Data.DeliveryPointGroupManager.WebAPI/Utils/DeliveryPointGroupConstants.cs
+++ RM/RM.Data.DeliveryPointGroupManager.WebAPI/Utils/DeliveryPointGroupConstants.cs
@@ -9,5 +9,6 @@
         internal const string Polygon = "POLYGON(({0} {1}, {2} {3}, {4} {5}, {6} {7}, {8} {9}))";
 
         internal const string DeliveryPointGroupDataProviderGUID = "RMGServiceNode";
+        internal const string PolygonWellKnownText = "POLYGON(({0}))";
     }
 }
