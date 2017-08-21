cfdc9da96078be125eac70ff55e59ec05d27f276
Pandey, Abhinav <abhinav.a.pandey@capgemini.com>
2017-08-21 11:53:37 +0100

    validate number component is added for RFMO-139

---
+++ RM/RM.Client/wwwroot/app/common/validate-number/validate-number.component.js
@@ -0,0 +1,6 @@
+﻿angular
+    .module('validateNumber')
+    .component('validateNumber', {
+        templateUrl: './common/validate-number/validate-number.template.html',
+        controller: 'ValidateNumberController as vm'
+    });
